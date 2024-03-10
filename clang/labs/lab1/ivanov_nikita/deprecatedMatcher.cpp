055893cf3d37200869db4520c166e888560c6a3f
Atikin-NT <ivanov.n.a.0620@gmail.com>
2024-03-10 20:09:56 +0300

    clang-format

--- clang/labs/lab1/ivanov_nikita/deprecatedMatcher.cpp
+++ clang/labs/lab1/ivanov_nikita/deprecatedMatcher.cpp
@@ -9,48 +9,51 @@ class DepConsumer : public ASTConsumer {
   CompilerInstance &Instance;
 
 public:
-    explicit DepConsumer(CompilerInstance &CI): Instance(CI) {}
+  explicit DepConsumer(CompilerInstance &CI) : Instance(CI) {}
 
-    void HandleTranslationUnit(ASTContext &Context) override {
-        struct Visitor : public RecursiveASTVisitor<Visitor> {
-          ASTContext *Context;
+  void HandleTranslationUnit(ASTContext &Context) override {
+    struct Visitor : public RecursiveASTVisitor<Visitor> {
+      ASTContext *Context;
 
-          Visitor(ASTContext *Context): Context(Context) {}
-          
-          bool VisitFunctionDecl(FunctionDecl *Func) {
-            if (Func->getNameInfo().getAsString().find("deprecated") != std::string::npos) {
-                DiagnosticsEngine &Diags = Context->getDiagnostics();
-                unsigned DiagID = Diags.getCustomDiagID(DiagnosticsEngine::Warning,
-                                                      "Deprecated in function name");
-                Diags.Report(Func->getLocation(), DiagID) << Func->getNameInfo().getAsString();
-            }
-            return true;
-          }
-        } v(&Instance.getASTContext());
+      Visitor(ASTContext *Context) : Context(Context) {}
 
-        v.TraverseDecl(Context.getTranslationUnitDecl());
-    }
+      bool VisitFunctionDecl(FunctionDecl *Func) {
+        if (Func->getNameInfo().getAsString().find("deprecated") !=
+            std::string::npos) {
+          DiagnosticsEngine &Diags = Context->getDiagnostics();
+          unsigned DiagID = Diags.getCustomDiagID(
+              DiagnosticsEngine::Warning, "Deprecated in function name");
+          Diags.Report(Func->getLocation(), DiagID)
+              << Func->getNameInfo().getAsString();
+        }
+        return true;
+      }
+    } v(&Instance.getASTContext());
+
+    v.TraverseDecl(Context.getTranslationUnitDecl());
+  }
 };
 
 class DeprecatedPlugin : public PluginASTAction {
 protected:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer (
-    CompilerInstance &Compiler, llvm::StringRef InFile) override {
-      return std::make_unique<DepConsumer>(Compiler);
-    }
+  std::unique_ptr<ASTConsumer>
+  CreateASTConsumer(CompilerInstance &Compiler,
+                    llvm::StringRef InFile) override {
+    return std::make_unique<DepConsumer>(Compiler);
+  }
 
-  bool ParseArgs(const CompilerInstance &Compiler, 
+  bool ParseArgs(const CompilerInstance &Compiler,
                  const std::vector<std::string> &args) override {
     if (!args.empty() && args[0] == "help")
       PrintHelp(llvm::errs());
-    
+
     return true;
   }
 
-  void PrintHelp(llvm::raw_ostream& ros) {
+  void PrintHelp(llvm::raw_ostream &ros) {
     ros << "Deprecated Plugin version 1.0\n";
   }
 };
 
-static FrontendPluginRegistry::Add<DeprecatedPlugin>
-X("deprecated-match", "deprecated match");
+static FrontendPluginRegistry::Add<DeprecatedPlugin> X("deprecated-match",
+                                                       "deprecated match");
