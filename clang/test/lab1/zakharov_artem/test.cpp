10309486d733da22704e33ec4f3ad951335a4b73
Artem <uni744@yandex.ru>
2024-03-10 19:24:59 +0300

    Add rename plugin tests

---
+++ clang/test/lab1/zakharov_artem/test.cpp
@@ -0,0 +1,101 @@
+// RUN: split-file %s %t
+
+// RUN: %clang_cc1 -load %llvmshlibdir/RenamePlugin%pluginext\
+// RUN: -add-plugin rename\
+// RUN: -plugin-arg-rename type=var\
+// RUN: -plugin-arg-rename cur-name=a\
+// RUN: -plugin-arg-rename new-name=new_var %t/rename_var.cpp
+// RUN: diff %t/rename_var_expected.cpp %t/rename_var.cpp
+
+// RUN: %clang_cc1 -load %llvmshlibdir/RenamePlugin%pluginext\
+// RUN: -add-plugin rename\
+// RUN: -plugin-arg-rename type=func\
+// RUN: -plugin-arg-rename cur-name=function\
+// RUN: -plugin-arg-rename new-name=new_func %t/rename_func.cpp
+// RUN: diff %t/rename_func_expected.cpp %t/rename_func.cpp
+
+// RUN: %clang_cc1 -load %llvmshlibdir/RenamePlugin%pluginext\
+// RUN: -add-plugin rename\
+// RUN: -plugin-arg-rename type=class\
+// RUN: -plugin-arg-rename cur-name=Base\
+// RUN: -plugin-arg-rename new-name=SimpleClass %t/rename_class.cpp
+// RUN: diff %t/rename_class_expected.cpp %t/rename_class.cpp
+
+// RUN: %clang_cc1 -load %llvmshlibdir/RenamePlugin%pluginext\
+// RUN: -add-plugin rename\
+// RUN: -plugin-arg-rename help\
+// RUN: 2>&1 | FileCheck %s --check-prefix=HELP
+
+// HELP: Specify three required arguments:
+// HELP: -plugin-arg-rename type=["var", "func", "class"]
+// HELP: -plugin-arg-rename cur-name="Current identifier name"
+// HELP: -plugin-arg-rename new-name="New identifier name"
+
+//--- rename_var.cpp
+int func() {
+  int a = 2, b = 2;
+  a = b + a;
+  a++;
+  return a;
+}
+//--- rename_var_expected.cpp
+int func() {
+  int new_var = 2, b = 2;
+  new_var = b + new_var;
+  new_var++;
+  return new_var;
+}
+//--- rename_func.cpp
+int function(int param) {
+    int a;
+    a = 2;
+    return param + a;
+}
+int other_func(){
+  function(3);
+  int a = function(2) + 3;
+  return a;
+}
+//--- rename_func_expected.cpp
+int new_func(int param) {
+    int a;
+    a = 2;
+    return param + a;
+}
+int other_func(){
+  new_func(3);
+  int a = new_func(2) + 3;
+  return a;
+}
+//--- rename_class.cpp
+class Base{
+ private:
+  int a;
+  int b;
+ public:
+  Base() {}
+  Base(int a, int b): a(a), b(b) {}
+  ~Base();
+};
+
+void func() {
+  Base a;
+  Base* var = new Base(1, 2);
+  delete var;
+}
+//--- rename_class_expected.cpp
+class SimpleClass{
+ private:
+  int a;
+  int b;
+ public:
+  SimpleClass() {}
+  SimpleClass(int a, int b): a(a), b(b) {}
+  ~SimpleClass();
+};
+
+void func() {
+  SimpleClass a;
+  SimpleClass* var = new SimpleClass(1, 2);
+  delete var;
+}
