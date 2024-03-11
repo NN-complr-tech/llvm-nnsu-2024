4e8576b8f85b86aef827e6ee51cc0fbc538ece40
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-03-11 13:37:52 +0300

    add visitor

--- clang/labs/lab1/kuznetsov_artyom/PrintClassMembers.cpp
+++ clang/labs/lab1/kuznetsov_artyom/PrintClassMembers.cpp
@@ -4,3 +4,20 @@
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 
+using namespace clang;
+using namespace llvm;
+
+namespace {
+class PrintClassMembersVisitor final
+    : public RecursiveASTVisitor<PrintClassMembersVisitor> {
+public:
+  explicit PrintClassMembersVisitor(ASTContext *context) : m_context(context) {}
+  bool VisitCXXRecordDecl(CXXRecordDecl *declaration) {
+    declaration->dump();
+    return true;
+  }
+
+private:
+  ASTContext *m_context;
+};
+}
