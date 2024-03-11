7aaa8f306efc4dd92509beae1af127b44e3a36b6
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-03-11 16:40:54 +0300

    upgrade task

--- clang/labs/lab1/kuznetsov_artyom/PrintClassMembers.cpp
+++ clang/labs/lab1/kuznetsov_artyom/PrintClassMembers.cpp
@@ -1,32 +1,67 @@
-#include "clang/AST/AST.h"
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
-
-using namespace clang;
-using namespace llvm;
+#include "llvm/Support/raw_ostream.h"
 
 namespace {
 class PrintClassMembersVisitor final
-    : public RecursiveASTVisitor<PrintClassMembersVisitor> {
+    : public clang::RecursiveASTVisitor<PrintClassMembersVisitor> {
 public:
-  explicit PrintClassMembersVisitor(ASTContext *context) : m_context(context) {}
-  bool VisitCXXRecordDecl(CXXRecordDecl *declaration) {
-    declaration->dump();
+  explicit PrintClassMembersVisitor(clang::ASTContext *context)
+      : m_context(context) {}
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *declaration) {
+    if (declaration->isStruct() || declaration->isClass()) {
+      outInfoUserType(declaration);
+
+      auto members = declaration->fields();
+
+      for (const auto &member : members) {
+        llvm::outs() << "|_ " << member->getNameAsString() << ' ';
+        llvm::outs() << '(' << getTypeString(member) << '|';
+        llvm::outs() << getAccessSpecifierAsString(member) << ")\n";
+      }
+
+      llvm::outs() << '\n';
+    }
     return true;
   }
 
 private:
-  ASTContext *m_context;
+  void outInfoUserType(clang::CXXRecordDecl *userType) {
+    llvm::outs() << userType->getNameAsString() << ' ';
+    llvm::outs() << (userType->isStruct() ? "(struct" : "(class");
+    llvm::outs() << (userType->isTemplated() ? "|template)" : ")") << '\n';
+  }
+
+  std::string getTypeString(const clang::FieldDecl *member) {
+    clang::QualType type = member->getType();
+    return type.getAsString();
+  }
+
+  std::string getAccessSpecifierAsString(const clang::FieldDecl *member) {
+    switch (member->getAccess()) {
+    case clang::AS_public:
+      return "public";
+    case clang::AS_protected:
+      return "protected";
+    case clang::AS_private:
+      return "private";
+    default:
+      return "unknown";
+    }
+  }
+
+private:
+  clang::ASTContext *m_context;
 };
 
-class PrintClassMembersConsumer final : public ASTConsumer {
+class PrintClassMembersConsumer final : public clang::ASTConsumer {
 public:
-  explicit PrintClassMembersConsumer(ASTContext *сontext)
+  explicit PrintClassMembersConsumer(clang::ASTContext *сontext)
       : m_visitor(сontext) {}
 
-  void HandleTranslationUnit(ASTContext &context) override {
+  void HandleTranslationUnit(clang::ASTContext &context) override {
     m_visitor.TraverseDecl(context.getTranslationUnitDecl());
   }
 
@@ -34,19 +69,19 @@ private:
   PrintClassMembersVisitor m_visitor;
 };
 
-class PrintClassMembersAction final : public PluginASTAction {
+class PrintClassMembersAction final : public clang::PluginASTAction {
 public:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci,
-                                                 StringRef) override {
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
     return std::make_unique<PrintClassMembersConsumer>(&ci.getASTContext());
   }
 
-  bool ParseArgs(const CompilerInstance &ci,
+  bool ParseArgs(const clang::CompilerInstance &ci,
                  const std::vector<std::string> &args) override {
     return true;
   }
 };
 } // namespace
 
-static FrontendPluginRegistry::Add<PrintClassMembersAction>
+static clang::FrontendPluginRegistry::Add<PrintClassMembersAction>
     X("pcm_plugin", "Prints all members of the class");
--- clang/test/lab1/kuznetsov_artyom/test.cpp
+++ clang/test/lab1/kuznetsov_artyom/test.cpp
@@ -1,11 +1,38 @@
 
-
-int factorial(int value) {
-  return value <= 1 ? 1 : value * factorial(value - 1);
-}
-
 struct Point3D {
   double m_x{};
   double m_y{};
   double m_z{};
+};
+
+template <typename T1, typename T2> struct Pair {
+  T1 first{};
+  T2 second{};
+};
+
+struct User {
+  int m_id{};
+
+  struct Human {
+    int m_age{};
+    int m_cash{};
+  };
+};
+
+class EmptyClass {};
+
+template <typename T> struct NodeList {
+  T data{};
+  NodeList<T> *next{};
+};
+
+class CheckSpecifiers {
+public:
+  Point3D publicMember{};
+
+protected:
+  User::Human protectedMember{};
+
+private:
+  EmptyClass privateMember{};
 };
