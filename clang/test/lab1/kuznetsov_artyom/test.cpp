930db5677764230c461a9ffef10da74238602867
Kuznetsov-Artyom <artyomka_working@mail.ru>
2024-03-11 17:17:24 +0300

    done

--- clang/test/lab1/kuznetsov_artyom/test.cpp
+++ clang/test/lab1/kuznetsov_artyom/test.cpp
@@ -1,38 +1,62 @@
+// RUN: %clang_cc1 -load %llvmshlibdir/PrintClassMembersPlugin%pluginext -plugin pcm_plugin %s 1>&1 | FileCheck %s
 
+namespace {
+// CHECK: Point3D (struct)
 struct Point3D {
+  // CHECK-NEXT: |_ m_x (double|public)
   double m_x{};
+  // CHECK-NEXT: |_ m_y (double|public)
   double m_y{};
+  // CHECK-NEXT: |_ m_z (double|public)
   double m_z{};
 };
 
+// CHECK: Pair (struct|template)
 template <typename T1, typename T2> struct Pair {
+  // CHECK-NEXT: |_ first (T1|public)
   T1 first{};
+  // CHECK-NEXT: |_ second (T2|public)
   T2 second{};
 };
 
-struct User {
-  int m_id{};
-
-  struct Human {
-    int m_age{};
-    int m_cash{};
-  };
-};
-
+// CHECK: EmptyClass (class)
 class EmptyClass {};
 
+// CHECK: NodeList (struct|template)
 template <typename T> struct NodeList {
+  // CHECK-NEXT: |_ data (T|public)
   T data{};
+  // CHECK-NEXT: |_ next (NodeList<T> *|public)
   NodeList<T> *next{};
 };
 
+// CHECK: CheckSpecifiers (class)
 class CheckSpecifiers {
 public:
+  // CHECK-NEXT: |_ publicMember (Point3D|public)
   Point3D publicMember{};
 
 protected:
-  User::Human protectedMember{};
+  // CHECK-NEXT: |_ protectedMember (Pair<int, NodeList<Pair<short, char> >
+  // >|protected)
+  Pair<int, NodeList<Pair<short, char>>> protectedMember{};
 
 private:
+  // CHECK-NEXT: |_ privateMember (EmptyClass|private)
   EmptyClass privateMember{};
 };
+
+// CHECK: User (struct)
+struct User {
+  // CHECK-NEXT: |_ m_id (int|public)
+  int m_id{};
+
+  // CHECK: Human (struct)
+  struct Human {
+    // CHECK-NEXT: |_ m_age (int|public)
+    int m_age{};
+    // CHECK-NEXT: |_ m_cash (int|public)
+    int m_cash{};
+  };
+};
+} // namespace
