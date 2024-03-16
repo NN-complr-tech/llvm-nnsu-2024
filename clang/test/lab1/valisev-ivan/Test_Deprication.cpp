2a6862e994147aa0230829acabb241dec69758ba
Иван Васильев ФИИТ1 <Vanovano03@mail.ru>
2024-03-16 23:17:09 +0300

    Try 2

--- clang/test/lab1/valisev-ivan/Test_Deprication.cpp
+++ clang/test/lab1/valisev-ivan/Test_Deprication.cpp
@@ -1,16 +1,16 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/DeprecationPlugin%pluginext -plugin deprecation-plugin %s 2>&1 | FileCheck %s
 
 // CHECK-NOT: warning: Deprecated function found here
-void somebody();
+int somebody();
 
 // CHECK-NOT: warning: Deprecated function found here
-void oncedepre();
+int oncedepre();
 
 // CHECK: warning: Deprecated function found here
-void toldeprecatedme();
+int toldeprecatedme();
 
 // CHECK-NOT: warning: Deprecated function found here
-void theworld();
+int theworld();
 
 // CHECK: warning: Deprecated function found here
-void deprecatedme();
+int deprecatedme();
