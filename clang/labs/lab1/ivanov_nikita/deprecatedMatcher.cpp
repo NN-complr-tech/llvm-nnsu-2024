b2cb5c278d46018648c5cf2775c22ccfe4c6d85b
Arseniy Obolenskiy <gooddoog@student.su>
2024-03-17 18:16:14 +0800

    Update deprecatedMatcher.cpp

--- clang/labs/lab1/ivanov_nikita/deprecatedMatcher.cpp
+++ clang/labs/lab1/ivanov_nikita/deprecatedMatcher.cpp
@@ -9,9 +9,7 @@ class DepConsumer : public ASTConsumer {
   CompilerInstance &Instance;
 
 public:
-  explicit DepConsumer(CompilerInstance &CI) : Instance(CI) {
-    
-  }
+  explicit DepConsumer(CompilerInstance &CI) : Instance(CI) {}
 
   void HandleTranslationUnit(ASTContext &Context) override {
     struct Visitor : public RecursiveASTVisitor<Visitor> {
