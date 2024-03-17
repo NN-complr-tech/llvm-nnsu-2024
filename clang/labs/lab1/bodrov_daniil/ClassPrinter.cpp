d70e74eb5d79df3aaac77c0a7f18873d0cd120fd
overinvest <r1gzhvhkid@gmail.com>
2024-03-17 12:15:44 +0300

    Final ver

--- clang/labs/lab1/bodrov_daniil/ClassPrinter.cpp
+++ clang/labs/lab1/bodrov_daniil/ClassPrinter.cpp
@@ -1,49 +1,97 @@
 #include "clang/AST/ASTConsumer.h"
-#include "clang/AST/ASTContext.h"
-#include "clang/AST/Decl.h"
-#include "clang/AST/DeclCXX.h"
-#include "clang/ASTMatchers/ASTMatchers.h"
-#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "llvm/Support/raw_ostream.h"
 
-using namespace clang;
-using namespace clang::ast_matchers;
+namespace {
 
-class ClassFieldPrinter : public MatchFinder::MatchCallback {
+class MemberInfoPrinter {
 public:
-  virtual void run(const MatchFinder::MatchResult &Result) {
-    if (const CXXRecordDecl *ClassDecl =
-            Result.Nodes.getNodeAs<CXXRecordDecl>("class")) {
-      llvm::outs() << ClassDecl->getNameAsString() << "\n";
-      for (const auto Field : ClassDecl->fields()) {
-        llvm::outs() << "  |_ " << Field->getNameAsString() << "\n";
-      }
+  void print(const clang::ValueDecl *member, const std::string &memberType) {
+    llvm::outs() << "|_ " << member->getNameAsString() << ' ';
+    llvm::outs() << '(' << member->getType().getAsString() << '|';
+    llvm::outs() << getAccessSpecifierAsString(member) << (memberType == "field" ? ")" : ("|" + memberType + ")") ) << "\n";
+  }
+
+private:
+  std::string getAccessSpecifierAsString(const clang::ValueDecl *member) {
+    switch (member->getAccess()) {
+    case clang::AS_public:
+      return "public";
+    case clang::AS_protected:
+      return "protected";
+    case clang::AS_private:
+      return "private";
+    default:
+      return "unknown";
     }
   }
 };
 
-class ClassFieldPrinterAction : public PluginASTAction {
-protected:
-  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
-                                                  llvm::StringRef) override {
-    Finder.addMatcher(
-        cxxRecordDecl(hasDefinition(), unless(isImplicit())).bind("class"),
-        &Printer);
-    return std::make_unique<ASTConsumer>();
+class UserTypePrinter {
+public:
+  void print(clang::CXXRecordDecl *userType) {
+    llvm::outs() << userType->getNameAsString() << ' ';
+    llvm::outs() << (userType->isStruct() ? "(struct" : "(class");
+    llvm::outs() << (userType->isTemplated() ? "|template)" : ")") << '\n';
   }
+};
 
-  bool ParseArgs(const CompilerInstance &CI,
-                 const std::vector<std::string> &args) override {
+class ClassMembersPrinter final : public clang::RecursiveASTVisitor<ClassMembersPrinter> {
+public:
+  explicit ClassMembersPrinter(clang::ASTContext *context) : context_(context) {}
+
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *declaration) {
+    if (declaration->isStruct() || declaration->isClass()) {
+      userTypePrinter_.print(declaration);
+
+      for (const auto &decl : declaration->decls()) {
+        if (auto field = llvm::dyn_cast<clang::FieldDecl>(decl)) {
+          memberInfoPrinter_.print(field, "field");
+        } else if (auto var = llvm::dyn_cast<clang::VarDecl>(decl)) {
+          if (var->isStaticDataMember()) {
+            memberInfoPrinter_.print(var, "static");
+          }
+        } else if (auto method = llvm::dyn_cast<clang::CXXMethodDecl>(decl)) {
+          memberInfoPrinter_.print(method, "method");
+        }
+      }
+      llvm::outs() << '\n';
+    }
     return true;
   }
 
 private:
-  MatchFinder Finder;
-  ClassFieldPrinter Printer;
+  clang::ASTContext *context_;
+  MemberInfoPrinter memberInfoPrinter_;
+  UserTypePrinter userTypePrinter_;
 };
 
-static FrontendPluginRegistry::Add<ClassFieldPrinterAction>
-    X("class-field-printer", "print class names and fields");
+class ClassMembersConsumer final : public clang::ASTConsumer {
+public:
+  explicit ClassMembersConsumer(clang::ASTContext *context) : visitor_(context) {}
 
+  void HandleTranslationUnit(clang::ASTContext &context) override {
+    visitor_.TraverseDecl(context.getTranslationUnitDecl());
+  }
+
+private:
+  ClassMembersPrinter visitor_;
+};
+
+class ClassFieldPrinterAction final : public clang::PluginASTAction {
+public:
+  std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &ci, llvm::StringRef) override {
+    return std::make_unique<ClassMembersConsumer>(&ci.getASTContext());
+  }
+
+  bool ParseArgs(const clang::CompilerInstance &ci, const std::vector<std::string> &args) override {
+    return true;
+  }
+};
+
+} // namespace
+
+static clang::FrontendPluginRegistry::Add<ClassFieldPrinterAction>
+    X("class-field-printer", "Prints all members of the class");
--- clang/test/lab1/bodrov_daniil/test.cpp
+++ clang/test/lab1/bodrov_daniil/test.cpp
@@ -1,16 +1,69 @@
-class MyClass {
+// RUN: %clang_cc1 -load %llvmshlibdir/ClassFieldPrinter%pluginext -plugin class-field-printer %s 1>&1 | FileCheck %s
+
+// CHECK: Empty (class)
+class Empty {};
+
+// CHECK: Data (class)
+class Data {
 public:
-  int a;
-  float b;
+  int a; // CHECK-NEXT: |_ a (int|public)
+  static int b; // CHECK-NEXT: |_ b (int|public|static)
+  void func() {} // CHECK-NEXT: |_ func (void (void)|public|method)
 };
 
+// CHECK: Template (class|template)
+template <typename T>
+class Template {
+public:
+  T value; // CHECK-NEXT: |_ value (T|public)
+};
+
+// CHECK: TestClass (class)
+class TestClass {
+public:
+  int publicInt; // CHECK-NEXT: |_ publicInt (int|public)
+  static int publicStaticInt; // CHECK-NEXT: |_ publicStaticInt (int|public|static)
+  void publicFunc() {} // CHECK-NEXT: |_ publicFunc (void (void)|public|method)
+
+private:
+  int privateInt; // CHECK-NEXT: |_ privateInt (int|private)
+  static int privateStaticInt; // CHECK-NEXT: |_ privateStaticInt (int|private|static)
+  void privateFunc() {} // CHECK-NEXT: |_ privateFunc (void (void)|private|method)
+};
+
+// CHECK: AnotherTestClass (class)
+class AnotherTestClass {
+public:
+  double publicDouble; // CHECK-NEXT: |_ publicDouble (double|public)
+  static double publicStaticDouble; // CHECK-NEXT: |_ publicStaticDouble (double|public|static)
+  double publicFunc(); // CHECK-NEXT: |_ publicFunc (double (void)|public|method)
+
+  Template<TestClass> t_TestClass; // CHECK-NEXT: |_ t_TestClass (Template<TestClass>|public)
+
+private:
+  double privateDouble; // CHECK-NEXT: |_ privateDouble (double|private)
+  static double privateStaticDouble; // CHECK-NEXT: |_ privateStaticDouble (double|private|static)
+  float privateFunc(const char* str); // CHECK-NEXT: privateFunc (float (const char *)|private|method)
+};
+
+// CHECK: DerivedClass (class)
+class DerivedClass : public TestClass {
+public:
+  int derivedPublicInt; // CHECK-NEXT: |_ derivedPublicInt (int|public)
+  static int derivedPublicStaticInt; // CHECK-NEXT: |_ derivedPublicStaticInt (int|public|static)
+  TestClass derivedPublicFunc(Template<int> data, Template<const char*> data_str); // CHECK-NEXT: |_ derivedPublicFunc (TestClass (Template<int>, Template<const char *>)|public|method)
+
+  AnotherTestClass classField; // CHECK-NEXT: |_ classField (AnotherTestClass|public)
+
+private:
+  int derivedPrivateInt; // CHECK-NEXT: |_ derivedPrivateInt (int|private)
+  static int derivedPrivateStaticInt; // CHECK-NEXT: |_ derivedPrivateStaticInt (int|private|static)
+  void derivedPrivateFunc(); // CHECK-NEXT: |_ derivedPrivateFunc (void (void)|private|method)
+  Template<Template<TestClass>> t_TestClass; // CHECK-NEXT: |_ t_TestClass (Template<Template<TestClass> >|private)
+};
+
+// CHECK: MyStruct (struct)
 struct MyStruct {
-  int a;
-  float b;
+  int x; // CHECK-NEXT: |_ x (int|public)
+  double y; // CHECK-NEXT: |_ y (double|public)
 };
-
-void myFunction() {
-  MyClass obj;
-  obj.a = 5;
-  obj.b = 3.14;
-}
