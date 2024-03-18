02e1cba1b3a4ba898141c6d84446cdaa3fb27d32
Kirill23323 <90023656+Kirill23323@users.noreply.github.com>
2024-03-18 07:39:56 +0300

    Update test.cpp

--- clang/test/lab1/prokofev_kirill/test.cpp
+++ clang/test/lab1/prokofev_kirill/test.cpp
@@ -4,10 +4,11 @@
 // RUN: -plugin-arg-renameplug newName="Beta" %t/class_test.cpp
 // RUN: FileCheck %s < %t/class_test.cpp --check-prefix=CLASS
 
+
 // CLASS: class Beta {
 // CLASS-NEXT: public:
-// CLASS-NEXT:   Beta() {};
-// CLASS-NEXT:    ~Beta();
+// CLASS-NEXT:  Beta(){};
+// CLASS-NEXT:  ~Beta();
 // CLASS-NEXT:};
 
 //--- class_test.cpp
@@ -22,9 +23,7 @@ public:
 // RUN: -plugin-arg-renameplug newName="Function" %t/function_test.cpp
 // RUN: FileCheck %s < %t/function_test.cpp --check-prefix=FUNCTION
 
-// FUNCTION: int Function(int digit) {
-// FUNCTION-NEXT:     return digit * 2;
-// FUNCTION-NEXT: }
+// FUNCTION: int Function(int digit) { return digit * 2; };
 
 //--- function_test.cpp
 int Foo(int digit) { return digit * 2; };
@@ -34,10 +33,9 @@ int Foo(int digit) { return digit * 2; };
 // RUN: -plugin-arg-renameplug newName="newVar" %t/variable_test.cpp
 // RUN: FileCheck %s < %t/variable_test.cpp --check-prefix=VARIABLE
 
-// VARIABLE: int Foo(int newVar) {
-// VARIABLE-NEXT:     return newVar * 2;
-// VARIABLE-NEXT: }
+// VARIABLE: Foo(int newVar) { return newVar * 2; }
 
 //--- variable_test.cpp
 int Foo(int oldVar) { return oldVar * 2; }
+
 
