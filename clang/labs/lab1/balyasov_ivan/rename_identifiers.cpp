57dc6e94e1432d0cb62367e4c88d70fc7c29bd4e
DiPirs <admarhanck@gmail.com>
2024-03-18 19:41:26 +0300

    change style again (x5)

--- clang/labs/lab1/balyasov_ivan/rename_identifiers.cpp
+++ clang/labs/lab1/balyasov_ivan/rename_identifiers.cpp
@@ -102,7 +102,7 @@ public:
   explicit RenameConsumer(clang::CompilerInstance &CI, Types type,
                           clang::StringRef oldName, clang::StringRef newName)
       : Visitor(clang::Rewriter(CI.getSourceManager(), CI.getLangOpts()), type,
-                +oldName, newName) {}
+                oldName, newName) {}
 
   void HandleTranslationUnit(clang::ASTContext &context) override {
     Visitor.TraverseDecl(context.getTranslationUnitDecl());
