a0c6ad10ed0c975b8595bf71ea766a1d249e2bfc
Иван Васильев ФИИТ1 <Vanovano03@mail.ru>
2024-03-19 01:55:19 +0300

    Fixed name in files, add option

--- clang/labs/lab1/vasilev_ivan/Find_Deprecation.cpp
+++ clang/labs/lab1/vasilev_ivan/Find_Deprecation.cpp
@@ -1,7 +1,28 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "llvm/ADT/StringRef.h"
+#include <algorithm>
+#include <cctype>
+#include <memory>
+#include <string>
+#include <vector>
+
+bool CaseSensitiveSearch = true;
+
+bool contains_ci(std::string const &text, std::string const &substr) {
+  if (substr.length() > text.length())
+    return false;
+
+  auto it = std::search(text.begin(), text.end(), substr.begin(), substr.end(),
+                        [](char ch1, char ch2) {
+                          return std::toupper(ch1) == std::toupper(ch2);
+                        });
+
+  return it != text.end();
+}
 
 class DeprecationWarnConsumer : public clang::ASTConsumer {
 public:
@@ -15,7 +36,11 @@ public:
           : Context(Context) {}
 
       bool VisitFunctionDecl(clang::FunctionDecl *Func) {
-        if (Func->getNameAsString().find("deprecated") != std::string::npos) {
+        bool check = CaseSensitiveSearch
+                         ? Func->getNameAsString().find("deprecated") !=
+                               std::string::npos
+                         : contains_ci(Func->getNameAsString(), "deprecated");
+        if (check) {
           clang::DiagnosticsEngine &Diags = Context.getDiagnostics();
           unsigned DiagID =
               Diags.getCustomDiagID(clang::DiagnosticsEngine::Warning,
@@ -40,6 +65,11 @@ public:
 protected:
   bool ParseArgs(const clang::CompilerInstance &Compiler,
                  const std::vector<std::string> &args) override {
+    for (unsigned i = 0, e = args.size(); i != e; ++i) {
+      if (args[i] == "insensitive") {
+        CaseSensitiveSearch = false;
+      }
+    }
     return true;
   }
 };
--- clang/test/lab1/valisev-ivan/Test_Deprication.cpp
+++ clang/test/lab1/valisev-ivan/Test_Deprication.cpp
@@ -14,3 +14,14 @@ int theworld();
 
 // CHECK: warning: Deprecated function found here
 int deprecatedme();
+
+// RUN: %clang_cc1 -load %llvmshlibdir/DeprecationPlugin%pluginext -plugin deprecation-plugin -plugin-arg-deprecation-plugin insensitive %s 2>&1 | FileCheck %s --check-prefix=INSENS
+
+// INSENS-NOT: warning: Deprecated function found here
+int theworld();
+
+// INSENS: warning: Deprecated function found here
+int deprecatedme();
+
+// INSENS: warning: Deprecated function found here
+int dePrecatedme();
