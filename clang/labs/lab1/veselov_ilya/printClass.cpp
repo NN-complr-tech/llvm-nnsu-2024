7e8cc07c35e7cafcd639394be6dbcf66dbabfc49
spells39 <spells228@gmail.com>
2024-03-19 00:05:13 +0300

    Veselov Ilya var 1

---
+++ clang/labs/lab1/veselov_ilya/printClass.cpp
@@ -0,0 +1,45 @@
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+
+class PrintClassVisitor : public clang::RecursiveASTVisitor<PrintClassVisitor> {
+public:
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *decl) {
+    if (decl->isStruct() || decl->isClass()) {
+      llvm::outs() << decl->getNameAsString() << "\n";
+      for (auto field : decl->fields()) {
+        llvm::outs() << "|_" << field->getNameAsString() << "\n";
+      }
+      llvm::outs() << "\n";
+    }
+    return true;
+  }
+};
+
+class PrintClassConsumer : public clang::ASTConsumer {
+public:
+  void HandleTranslationUnit(clang::ASTContext &Context) override {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
+
+private:
+  PrintClassVisitor Visitor;
+};
+
+class PrintClassPlugin : public clang::PluginASTAction {
+public:
+  std::unique_ptr<clang::ASTConsumer> CreateASTConsumer (
+    clang::CompilerInstance &Compiler, llvm::StringRef InFile) override {
+      return std::make_unique<PrintClassConsumer>();
+    }
+
+protected:
+  bool ParseArgs(const clang::CompilerInstance &Compiler, 
+                 const std::vector<std::string> &args) override {
+    return true;
+  }
+};
+
+static clang::FrontendPluginRegistry::Add<PrintClassPlugin>
+  X("print-class", "Prints description of class.");
