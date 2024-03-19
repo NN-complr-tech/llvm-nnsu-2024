1c7ee7a0100245bd45e64ea6aa4117552548b8b1
Alexseev Danila <user@DESKTOP-9FK8SDU.localdomain>
2024-03-20 00:43:58 +0300

    fix test2

--- clang/test/lab1/lysanova_julia/deprecatedTest.cpp
+++ clang/test/lab1/lysanova_julia/deprecatedTest.cpp
@@ -1,15 +1,15 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/LysanovaDepWarnPlugin%pluginext -plugin depWarning %s 2>&1 | FileCheck %s
 
-// CHECK: warning: This plugin throws warning if func name contains 'deprecated'
+// CHECK: warning: Function have deprecated in its name!
 void deprecated();
 
-// CHECK: warning: This plugin throws warning if func name contains 'deprecated'
+// CHECK: warning: Function have deprecated in its name!
 void deprecated123();
 
-// CHECK-NOT: warning: This plugin throws warning if func name contains 'deprecated'
+// CHECK-NOT: warning: Function have deprecated in its name!
 void abcdf();
 
-// CHECK-NOT: warning: This plugin throws warning if func name contains 'deprecated'
+// CHECK-NOT: warning: Function have deprecated in its name!
 void eprecated();
 
 // RUN: %clang_cc1 -load %llvmshlibdir/LysanovaDepWarnPlugin%pluginext -plugin depWarning -plugin-arg-depWarning -help %s 2>&1 | FileCheck %s
