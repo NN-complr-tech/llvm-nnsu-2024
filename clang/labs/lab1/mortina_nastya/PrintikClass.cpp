8925f5505d58ed044a728871a71790a96d40250e
NastyaMortina <mortinananika03@gmail.com>
2024-03-20 00:02:08 +0300

    Second commit

--- clang/labs/lab1/mortina_nastya/PrintikClass.cpp
+++ clang/labs/lab1/mortina_nastya/PrintikClass.cpp
@@ -4,44 +4,42 @@
 #include "clang/Frontend/FrontendPluginRegistry.h"
 #include "llvm/Support/raw_ostream.h"
 
-class PrintClassFieldsVisitor : public clang::RecursiveASTVisitor<PrintClassFieldsVisitor> {
+class PrintClassFieldsVisitor
+    : public clang::RecursiveASTVisitor<PrintClassFieldsVisitor> {
 public:
-    bool VisitCXXRecordDecl(clang::CXXRecordDecl *declaration) {
-        if (declaration->isClass() || declaration->isStruct()) {
-            llvm::outs() << "Class Name: " << declaration->getNameAsString() << "\n";
-            for (auto field : declaration->fields()) {
-                llvm::outs() << "|_" << field->getNameAsString() << "\n";
-            }
-            llvm::outs() << "\n";
-        }
-        return true;
+  bool VisitCXXRecordDecl(clang::CXXRecordDecl *declaration) {
+    if (declaration->isClass() || declaration->isStruct()) {
+      llvm::outs() << "Class Name: " << declaration->getNameAsString() << "\n";
+      for (auto field : declaration->fields()) {
+        llvm::outs() << "|_" << field->getNameAsString() << "\n";
+      }
+      llvm::outs() << "\n";
     }
+    return true;
+  }
 };
 
 class PrintClassFieldsConsumer : public clang::ASTConsumer {
 public:
-    PrintClassFieldsVisitor Visitor;
-  
-    void HandleTranslationUnit(clang::ASTContext &Context) override {
-        Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-    }
+  PrintClassFieldsVisitor Visitor;
+
+  void HandleTranslationUnit(clang::ASTContext &Context) override {
+    Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+  }
 };
 
 class PrintClassFieldsAction : public clang::PluginASTAction {
 public:
-    std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &CI,
-                                                 llvm::StringRef) override {
-        return std::make_unique<PrintClassFieldsConsumer>();
-    }
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &CI, llvm::StringRef) override {
+    return std::make_unique<PrintClassFieldsConsumer>();
+  }
 
-    bool ParseArgs(const clang::CompilerInstance &CI,
+  bool ParseArgs(const clang::CompilerInstance &CI,
                  const std::vector<std::string> &Args) override {
-        return true;
-    }
+    return true;
+  }
 };
 
 static clang::FrontendPluginRegistry::Add<PrintClassFieldsAction>
     X("prin-elds", "Prints names of all classes and their fields");
-    
-
- 
