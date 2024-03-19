d42c903bff7d64a1d9888510085fa4efe1fc08ae
Alexseev Danila <user@DESKTOP-9FK8SDU.localdomain>
2024-03-19 16:43:37 +0300

    clang-format fix

--- clang/labs/lab1/alexseev_danila/deprecatedWarning.cpp
+++ clang/labs/lab1/alexseev_danila/deprecatedWarning.cpp
@@ -16,7 +16,8 @@ public:
 
   bool VisitFunctionDecl(FunctionDecl *Func) {
     if (Func->getNameInfo().getAsString().find("deprecated") !=
-        std::string::npos && Func->getNameInfo().getAsString() != ExcludeFunc) {
+            std::string::npos &&
+        Func->getNameInfo().getAsString() != ExcludeFunc) {
       DiagnosticsEngine &Diags = Context->getDiagnostics();
       size_t CustomDiagID =
           Diags.getCustomDiagID(DiagnosticsEngine::Warning,
@@ -32,7 +33,8 @@ class DepFuncConsumer : public ASTConsumer {
   std::string ExcludeFunc;
 
 public:
-  explicit DepFuncConsumer(std::string ExcludeFunc) : ExcludeFunc(ExcludeFunc) {}
+  explicit DepFuncConsumer(std::string ExcludeFunc)
+      : ExcludeFunc(ExcludeFunc) {}
 
   void HandleTranslationUnit(ASTContext &Context) override {
     DepFuncVisitor Visitor(&Context, ExcludeFunc);
@@ -43,12 +45,11 @@ public:
 class DepFuncPlugin : public PluginASTAction {
 protected:
   std::string ExcludeFunc = "";
-  
+
   std::unique_ptr<ASTConsumer>
   CreateASTConsumer(CompilerInstance &Compiler,
                     llvm::StringRef InFile) override {
     return std::make_unique<DepFuncConsumer>(ExcludeFunc);
-
   }
   bool ParseArgs(const CompilerInstance &Compiler,
                  const std::vector<std::string> &Args) override {
@@ -56,7 +57,7 @@ protected:
       if (Args[i].substr(0, 9) == "-exclude=") {
         ExcludeFunc = Args[i].substr(9);
       }
-	}
+    }
     return true;
   }
 };
