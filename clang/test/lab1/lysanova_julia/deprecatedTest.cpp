34f858f99a9e158b8e0074580aba0bae155b2746
Alexseev Danila <user@DESKTOP-9FK8SDU.localdomain>
2024-03-20 00:10:55 +0300

    fix test

--- clang/test/lab1/lysanova_julia/deprecatedTest.cpp
+++ clang/test/lab1/lysanova_julia/deprecatedTest.cpp
@@ -1,25 +1,16 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/LysanovaDepWarnPlugin%pluginext -plugin depWarning %s 2>&1 | FileCheck %s
 
-// CHECK: warning: Function contains 'deprecated' in its name
+// CHECK: warning: This plugin throws warning if func name contains 'deprecated'
 void deprecated();
 
-// CHECK: warning: Function contains 'deprecated' in its name
+// CHECK: warning: This plugin throws warning if func name contains 'deprecated'
 void deprecated123();
 
-// CHECK: warning: Function contains 'deprecated' in its name
-void aaAdeprecatedOoo();
+// CHECK-NOT: warning: This plugin throws warning if func name contains 'deprecated'
+void abcdf();
 
-// CHECK-NOT: warning: Function contains 'deprecated' in its name
-void something();
-
-// CHECK-NOT: warning: Function contains 'deprecated' in its name
-void deprecatend();
-
-// CHECK-NOT: warning: Function contains 'deprecated' in its name
-void deprecate();
-
-// CHECK-NOT: warning: Function contains 'deprecated' in its name
-void deprecatedFunc();
+// CHECK-NOT: warning: This plugin throws warning if func name contains 'deprecated'
+void eprecated();
 
 // RUN: %clang_cc1 -load %llvmshlibdir/LysanovaDepWarnPlugin%pluginext -plugin depWarning -plugin-arg-depWarning -help %s 2>&1 | FileCheck %s
 
