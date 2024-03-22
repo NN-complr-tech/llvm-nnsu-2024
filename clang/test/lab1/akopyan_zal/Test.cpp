5c3a4ff33af693de4360bdada2e93b431b366dcb
Зал <fartyna-mercedes@mail.ru>
2024-03-22 16:27:25 +0300

    fixik

--- clang/test/lab1/akopyan_zal/Test.cpp
+++ clang/test/lab1/akopyan_zal/Test.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -ast-dump -ast-dump-filter -load %llvmshlibdir/AlwaysInlinePluginAkopyan%pluginext -plugin always-inline %s 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -ast-dump -ast-dump-filter -load %llvmshlibdir/AlwaysInlinePluginAkopyan%pluginext -add-plugin always-inline-plugin %s 2>&1 | FileCheck %s
 
 // CHECK: __attribute__((always_inline)) FuncInline
 int FuncInline(){ return 10; }
