0c7bd4d15477f0c9736c505b2ac601451db5b74d
Suren <simonyan-junior@yandex.ru>
2024-03-27 18:44:05 +0300

    Fix tests

--- clang/test/lab1/simonyan_suren/test.cpp
+++ clang/test/lab1/simonyan_suren/test.cpp
@@ -1,48 +1,80 @@
 // RUN: %clang_cc1 -ast-dump -ast-dump-filter testFun -load %llvmshlibdir/AlwaysInlinePlugin%pluginext -add-plugin always-inlines-plugin %s 2>&1 | FileCheck %s
 
-// CHECK: FunctionDecl {{.*}} testFunIf 'int \(int, int\)'
-int testFunIf(int A, int B) {
-  if (A < B) {
-    A = B;
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo1 'int \(\)'}}
+int __attribute__((always_inline)) foo1() { return 0; }
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo2 'int \(\)'}}
+int foo2() { return 0; }
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo3 'int \(\)'}}
+int foo3() {
+  bool f = true;
+  if (f) {
+    f = false;
   }
   return 0;
 }
-// CHECK-NOT: `-AlwaysInlineAttr {{.*}} Implicit always_inline`
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
 
-// CHECK: FunctionDecl {{.*}} testFunWhile 'void \(int, int\)'
-void testFunWhile(int A, int B) {
-  {
-    while(A < B) {
-      A = B;
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo4 'int \(\)'}}
+int foo4() {
+  while (true) {
+  }
+  return 0;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo5 'int \(\)'}}
+int foo5() {
+  for (int i = 0; i < 5; i++) {
+  }
+  return 0;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo6 'int \(\)'}}
+int foo6() {
+  while (true) {
+    if (false) {
+      int d = -1;
+      if (d) {
+        return d;
+      }
     }
   }
+  return 0;
 }
-// CHECK-NOT: `-AlwaysInlineAttr {{.*}} Implicit always_inline`
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
 
-// CHECK: FunctionDecl {{.*}} testFunEmpty 'int \(\)'
-int testFunEmpty() {}
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo7 'int \(\)'}}
+int foo7() {
+  do {
 
-// CHECK: FunctionDecl {{.*}} testFunFor 'int \(int\)'
-int testFunFor(int A) {
-  for (int i = 0; i < 0; i++){
+  } while (true);
+  return 0;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
 
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo8 'int \(\)'}}
+int foo8() {
+  int a = 1;
+  switch (a) {
+  case 1:
+    break;
+  default:
+    break;
   }
-
-  return A;
+  return a;
 }
-// CHECK-NOT: `-AlwaysInlineAttr {{.*}} Implicit always_inline`
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
 
-// CHECK: FunctionDecl {{.*}} testFunSwitch 'int \(int\)'
-int testFunSwitch(int A) {
-
-    switch (A)
-    {
-    case 1:
-      break;
-
-    default:
-      break;
-    }
-  return A;
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo9 'int \(int, int\)'}}
+int foo9(int a, int b) {
+  int c;
+  c = a + b;
+  return c;
 }
-// CHECK-NOT: `-AlwaysInlineAttr {{.*}} Implicit always_inline`
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
