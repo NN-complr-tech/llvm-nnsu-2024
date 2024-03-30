1971323199a9220fd8df0e89bf791a4b7a0f1e7f
Suren <simonyan-junior@yandex.ru>
2024-03-30 17:39:57 +0300

    Fix test

--- clang/test/lab1/simonyan_suren/test.cpp
+++ clang/test/lab1/simonyan_suren/test.cpp
@@ -19,10 +19,6 @@ void testFun2(int A, int B) {
 }
 // CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> Implicit always_inline}}
 
-// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) testFun3 'int \(\)'}}
-int testFun3() {return 0;}
-// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
-
 // CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) testFun4 'void \(char, char\)'}}
 void testFun4(char A, char B) {
   {
