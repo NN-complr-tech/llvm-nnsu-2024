713f1ce9169f3b3181cc36404e64170be7a0520d
Alex <x.kryack.x@gmail.com>
2024-03-30 23:58:24 +0300

    fix for mul

--- llvm/labs/lab2/kruglov_alexey/MulToBitShift.cpp
+++ llvm/labs/lab2/kruglov_alexey/MulToBitShift.cpp
@@ -15,35 +15,36 @@ public:
       std::stack<llvm::Instruction *> worklist;
       for (auto DI = BB.begin(); DI != BB.end();) {
         auto Inst = DI++;
-
-        if (auto *op = llvm::dyn_cast<llvm::BinaryOperator>(&*Inst)) {
-          llvm::IRBuilder<> builder(op);
-          llvm::Value *lhs = op->getOperand(0);
-          llvm::Value *rhs = op->getOperand(1);
-          int lg1 = -2, lg2 = -2;
-          if (llvm::ConstantInt *CIl = llvm::dyn_cast<llvm::ConstantInt>(lhs)) {
-            lg1 = CIl->getValue().exactLogBase2();
-          }
-          if (llvm::ConstantInt *CIr = llvm::dyn_cast<llvm::ConstantInt>(rhs)) {
-            lg2 = CIr->getValue().exactLogBase2();
-          }
-          if (lg1 < lg2) {
-            std::swap(lg1, lg2);
-            std::swap(lhs, rhs);
-          }
-          if (lg1 > -1) {
-            llvm::Value *val;
-            if (lg1 == 0)
-              val = rhs;
-            else {
-              val = builder.CreateShl(
-                  rhs, llvm::ConstantInt::get(op->getType(), lg1));
+        if (llvm::BinaryOperator *op = llvm::dyn_cast<llvm::BinaryOperator>(&*Inst)) {
+          if (op->getOpcodeName()=="mul") {
+            llvm::IRBuilder<> builder(op);
+            llvm::Value *lhs = op->getOperand(0);
+            llvm::Value *rhs = op->getOperand(1);
+            int lg1 = -2, lg2 = -2;
+            if (llvm::ConstantInt *CIl = llvm::dyn_cast<llvm::ConstantInt>(lhs)) {
+              lg1 = CIl->getValue().exactLogBase2();
             }
-            for (auto &U : op->uses()) {
-              llvm::User *user = U.getUser();
-              user->replaceUsesOfWith(op, val);
+            if (llvm::ConstantInt *CIr = llvm::dyn_cast<llvm::ConstantInt>(rhs)) {
+              lg2 = CIr->getValue().exactLogBase2();
             }
-            worklist.push(&*Inst);
+            if (lg1 < lg2) {
+              std::swap(lg1, lg2);
+              std::swap(lhs, rhs);
+            }
+            if (lg1 > -1) {
+              llvm::Value *val;
+              if (lg1 == 0)
+                val = rhs;
+              else {
+                val = builder.CreateShl(
+                    rhs, llvm::ConstantInt::get(op->getType(), lg1));
+              }
+              for (auto &U : op->uses()) {
+                llvm::User *user = U.getUser();
+                user->replaceUsesOfWith(op, val);
+              }
+              worklist.push(&*Inst);
+            }
           }
         }
       }
