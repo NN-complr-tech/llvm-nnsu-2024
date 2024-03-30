9be251cb074850658236c9a7c7885a8aa2c8bb56
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2024-03-30 17:25:44 +0300

    change solved

--- clang/test/lab1/kozyreva_ekaterina/tests.cpp
+++ clang/test/lab1/kozyreva_ekaterina/tests.cpp
@@ -32,5 +32,5 @@ class Test4 {
 // CHECK: Nested
 // CHECK-NEXT: |_z
 
-// RUN: %clang_cc1 -load %llvmshlibdir/kozyrevaPrintNamesPlugin%pluginext -plugin classprinter -plugin-arg-classprinter --help %s 2>&1 | FileCheck %s --check-prefix=HELP
+// RUN: not %clang_cc1 -load %llvmshlibdir/kozyrevaPrintNamesPlugin%pluginext -plugin classprinter -plugin-arg-classprinter --help %s 2>&1 | FileCheck %s --check-prefix=HELP
 // HELP: This plugin traverses the Abstract Syntax Tree (AST) of a codebase and prints the name and fields of each class it encounters
