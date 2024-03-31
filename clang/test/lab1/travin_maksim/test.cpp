5872c075172c106402cc9b71dc6bf0ed1339573f
Мfксим <stm_03@mail.ru>
2024-03-31 23:37:00 +0300

    2.0 fixes

--- clang/test/lab1/travin_maksim/test.cpp
+++ clang/test/lab1/travin_maksim/test.cpp
@@ -1,23 +1,25 @@
 // RUN: %clang_cc1 -ast-dump -ast-dump-filter test -load %llvmshlibdir/TravinAlwaysInlinePlugin%pluginext -add-plugin add-always-inline %s | FileCheck %s
 
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+// CHECK-NOT: `-AlwaysInlineAttr {{.* always_inline}}
+void __attribute__((always_inline)) test0(int a, int b) {
+    a = b;
+}
+
 // CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) test1 'void \(\)'}}
 void test1() {};
-
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
 
 // CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) test2 'int \(int\)'}}
 int test2(int a) {
 	return a;
 }
-
 // CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
-// CHECK-NOT: `-AlwaysInlineAttr {{.* always_inline}}
-__attribute__((always_inline)) void testInl(int a, int b) {
-    a = b;
-}
 
 // CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) test3 'int \(int, int\)'}}
 int test3(int a, int b) {
 	if (a > b) return 1;
+	return 0;
 }
 // CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> Implicit always_inline}}
 
@@ -36,5 +38,6 @@ int test5(int a, int b) {
 		case 2:
 			return b;
 	}
+	return 0;
 }
 // CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> Implicit always_inline}}
