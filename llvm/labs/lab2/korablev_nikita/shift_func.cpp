cb4f90aa9bf8507c38dabde8b92c3c8dd3ac9719
NikitaKorablev <kit06092003@gmail.com>
2024-04-01 22:44:40 +0300

    rework pass

--- llvm/labs/lab2/korablev_nikita/shift_func.cpp
+++ llvm/labs/lab2/korablev_nikita/shift_func.cpp
@@ -19,42 +19,22 @@ struct ReplaceMultToShift : llvm::PassInfoMixin<ReplaceMultToShift> {
     for (llvm::BasicBlock &BB : F) {
       for (llvm::Instruction &I : BB) {
         
-        // I.print(llvm::outs()); llvm::outs() << "\n";
-
-        // if (auto *BO = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
-        //   BO->print(llvm::outs()); llvm::outs() << "\n";
-        // }
-
-        // Является ли "I" бинарным оператом
-        // llvm::outs() << "---- BO start ----\n";
         if (llvm::BinaryOperator *BO = llvm::dyn_cast<llvm::BinaryOperator>(&I)) {
-          // llvm::outs() << "---- BO in ----\n";
-          // llvm::IRBuilder<> builder(BO);
-          
           if (BO->getOpcode() == llvm::Instruction::BinaryOps::Mul) {
-            // BO->getOperand(0)->print(llvm::outs()); llvm::outs() << "\n";
-            // BO->getOperand(1)->print(llvm::outs());
-            // llvm::outs() << "\nTEST1::::: "  << "\n";
-            llvm::ConstantInt *LHS = llvm::dyn_cast<llvm::ConstantInt>(BO->getOperand(0));
-            llvm::ConstantInt *RHS = llvm::dyn_cast<llvm::ConstantInt>(BO->getOperand(1));
+            llvm::ConstantInt *LOp = llvm::dyn_cast<llvm::ConstantInt>(BO->getOperand(0));
+            llvm::ConstantInt *ROp = llvm::dyn_cast<llvm::ConstantInt>(BO->getOperand(1));
 
-            if (LHS || RHS) {
+            if (LOp || ROp) {
               llvm::IRBuilder<> builder(BO);
-              // llvm::outs() << "---- IF in ----\n";
-              if (LHS != nullptr && LHS->getValue().isPowerOf2()) {
-                llvm::outs() << "---- LHS is not nullptr ----\n";
-                // llvm::Value *llvmValue = llvm::ConstantInt::get(llvm::IntegerType::get(F.getContext(), 32), llvm::APInt(32, LHS->getValue().exactLogBase2()));
-                BO->replaceAllUsesWith(llvm::BinaryOperator::Create(llvm::Instruction::Shl, BO->getOperand(0), BO->getOperand(1), "shiftInst", BO));
-                llvm::outs() << "---- LHS end ----\n";
-              } else if (RHS != nullptr && RHS->getValue().isPowerOf2()) {
-                llvm::outs() << "---- RHS is not nullptr ----\n";
-                // llvm::Value *llvmValue = llvm::ConstantInt::get(llvm::IntegerType::get(F.getContext(), 32), llvm::APInt(32, RHS->getValue().exactLogBase2()));
-                BO->replaceAllUsesWith(llvm::BinaryOperator::Create(llvm::Instruction::Shl, BO->getOperand(1), BO->getOperand(0), "shiftInst", BO));
-                // BO->replaceAllUsesWith(builder.CreateShl(BO->getOperand(1), llvm::ConstantInt::get(BO->getType(), lg1)));
-              // Удаляем Mul
-                llvm::outs() << "---- RHS end ----\n";
+              if (LOp != nullptr && LOp->getValue().isPowerOf2()) {
+                BO->replaceAllUsesWith(
+                  llvm::BinaryOperator::Create(llvm::Instruction::Shl, BO->getOperand(0), BO->getOperand(1), "shiftInst", BO)
+                );
+              } else if (ROp != nullptr && ROp->getValue().isPowerOf2()) {
+                BO->replaceAllUsesWith(
+                  llvm::BinaryOperator::Create(llvm::Instruction::Shl, BO->getOperand(1), BO->getOperand(0), "shiftInst", BO)
+                );
               }
-              // llvm::outs() << "---- IF out ----\n";
               inst_list.push(&I);
             }
           }
@@ -66,7 +46,6 @@ struct ReplaceMultToShift : llvm::PassInfoMixin<ReplaceMultToShift> {
       }
     }
     
-    // llvm::outs() << "---- Out start ----\n";
     auto PA = llvm::PreservedAnalyses::all();
     PA.abandon<llvm::LoopAnalysis>();
     return PA;
