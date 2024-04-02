67369636b78eae0ded857a12f4f54d24c8c423e7
Artem <uni744@yandex.ru>
2024-04-02 22:40:12 +0300

    Delete header file

--- llvm/labs/lab2/zakharov_artem/mtbs.cpp
+++ llvm/labs/lab2/zakharov_artem/mtbs.cpp
@@ -1,48 +1,53 @@
-#include "mtbs.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
 using namespace llvm;
 
-PreservedAnalyses MTBSPass::run(llvm::Function &F,
-                                llvm::FunctionAnalysisManager &AM) {
-  for (auto &BB : F) {
-    for (auto InstIt = BB.begin(); InstIt != BB.end(); InstIt++) {
-      if (InstIt->getOpcode() != Instruction::Mul) {
-        continue;
+namespace {
+
+class MTBSPass : public PassInfoMixin<MTBSPass> {
+public:
+  PreservedAnalyses run(llvm::Function &F, llvm::FunctionAnalysisManager &AM) {
+    for (auto &BB : F) {
+      for (auto InstIt = BB.begin(); InstIt != BB.end(); InstIt++) {
+        if (InstIt->getOpcode() != Instruction::Mul) {
+          continue;
+        }
+
+        auto *Op = dyn_cast<BinaryOperator>(InstIt);
+        Value *LVal = Op->getOperand(0);
+        Value *RVal = Op->getOperand(1);
+
+        int LLog = getLog2(LVal);
+        int RLog = getLog2(RVal);
+
+        if (RLog < LLog) {
+          std::swap(LLog, RLog);
+          std::swap(LVal, RVal);
+        }
+        if (RLog < 0) {
+          continue;
+        }
+
+        IRBuilder<> Builder(Op);
+        Value *NewVal =
+            Builder.CreateShl(LVal, ConstantInt::get(Op->getType(), RLog));
+        ReplaceInstWithValue(InstIt, NewVal);
       }
-
-      auto *Op = dyn_cast<BinaryOperator>(InstIt);
-      Value *LVal = Op->getOperand(0);
-      Value *RVal = Op->getOperand(1);
-
-      int LLog = getLog2(LVal);
-      int RLog = getLog2(RVal);
-
-      if (RLog < LLog) {
-        std::swap(LLog, RLog);
-        std::swap(LVal, RVal);
-      }
-      if (RLog < 0) {
-        continue;
-      }
-
-      IRBuilder<> Builder(Op);
-      Value *NewVal =
-          Builder.CreateShl(LVal, ConstantInt::get(Op->getType(), RLog));
-      ReplaceInstWithValue(InstIt, NewVal);
     }
+    return PreservedAnalyses::all();
   }
-  return PreservedAnalyses::all();
-}
 
-int MTBSPass::getLog2(llvm::Value *Op) {
-  if (auto *CI = dyn_cast<ConstantInt>(Op)) {
-    return CI->getValue().exactLogBase2();
+private:
+  int getLog2(llvm::Value *Op) {
+    if (auto *CI = dyn_cast<ConstantInt>(Op)) {
+      return CI->getValue().exactLogBase2();
+    }
+    return -1;
   }
-  return -1;
-}
+};
+} // namespace
 
 bool registerPipeLine(llvm::StringRef Name, llvm::FunctionPassManager &FPM,
                       llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
@@ -60,6 +65,8 @@ PassPluginLibraryInfo getMTBSPluginInfo() {
           }};
 }
 
+#ifndef LLVM_ZakharovMTBSPlugin_LINK_INTO_TOOLS
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
   return getMTBSPluginInfo();
 }
+#endif
