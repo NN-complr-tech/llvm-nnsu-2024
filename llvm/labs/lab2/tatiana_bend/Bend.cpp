60b954c38be4bf77a173362aa396a183bfef5023
Tatiana567 <90023891+Tatiana567@users.noreply.github.com>
2024-04-02 22:50:51 +0300

    Meow8

--- llvm/labs/lab2/tatiana_bend/Bend.cpp
+++ llvm/labs/lab2/tatiana_bend/Bend.cpp
@@ -26,7 +26,8 @@ struct Bend : llvm::PassInfoMixin<Bend> {
     insert_instr(F, instrument_callee);
   }
 
-  llvm::PreservedAnalyses run(llvm::Function &F, llvm::FunctionAnalysisManager &FAM) {
+  llvm::PreservedAnalyses run(llvm::Function &F,
+                              llvm::FunctionAnalysisManager &FAM) {
     llvm::LLVMContext &Context = F.getContext();
     llvm::IRBuilder<> Builder(Context);
 
