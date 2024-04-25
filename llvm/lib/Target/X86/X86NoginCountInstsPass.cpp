58690b977c03fd33224824995f7829b86b3e9692
Denchek <d.n0gin@yandex.ru>
2024-04-25 21:32:30 +0300

    update pass

--- llvm/lib/Target/X86/X86NoginCountInstsPass.cpp
+++ llvm/lib/Target/X86/X86NoginCountInstsPass.cpp
@@ -7,39 +7,41 @@
 using namespace llvm;
 
 class X86NoginCountInstsPass : public MachineFunctionPass {
- public:
+public:
   static inline char ID = 0;
 
   X86NoginCountInstsPass() : MachineFunctionPass(ID) {}
-
-  size_t countInstrBasBlock(MachineBasicBlock &BasicBlock) {
-    size_t Count = 0;
-    for (auto &CurInstr : BasicBlock) {
-      Count++;
-    }
-    return Count;
-  }
 
   bool runOnMachineFunction(MachineFunction &MachFunc) override {
     DebugLoc DebugLocation = MachFunc.front().begin()->getDebugLoc();
     const TargetInstrInfo *InstrInfo = MachFunc.getSubtarget().getInstrInfo();
     size_t CountInstsReg =
         MachFunc.getRegInfo().createVirtualRegister(&X86::GR64RegClass);
+    size_t Count = 0;
 
     for (auto &BasicBlock : MachFunc) {
-      size_t Count = countInstrBasBlock(BasicBlock);
-      BuildMI(BasicBlock, BasicBlock.getFirstTerminator(), DebugLocation,
-              InstrInfo->get(X86::ADD64ri32), CountInstsReg)
-          .addReg(CountInstsReg)
-          .addImm(Count);
+      Count += countInstrBasBlock(BasicBlock);
     }
 
+    BuildMI(MachFunc.back(), MachFunc.back().getFirstTerminator(),
+            DebugLocation, InstrInfo->get(X86::ADD64ri32), CountInstsReg)
+        .addReg(CountInstsReg)
+        .addImm(Count);
     BuildMI(MachFunc.back(), MachFunc.back().getFirstTerminator(),
             DebugLocation, InstrInfo->get(X86::MOV64mr))
         .addReg(CountInstsReg)
         .addExternalSymbol("ic");
 
     return true;
+  }
+
+private:
+  size_t countInstrBasBlock(MachineBasicBlock &BasicBlock) {
+    size_t Count = 0;
+    for (auto &CurInstr : BasicBlock) {
+      Count++;
+    }
+    return Count;
   }
 };
 
