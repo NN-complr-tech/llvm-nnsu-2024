024278fd3e8bdc186ab61372a80a4028215b6316
Manymoera <soloninko80@gmail.com>
2024-04-25 18:02:57 +0300

    style fixes

--- llvm/lib/Target/X86/X86SoloninkoOptsPass.cpp
+++ llvm/lib/Target/X86/X86SoloninkoOptsPass.cpp
@@ -6,28 +6,29 @@
 
 using namespace llvm;
 
-class X86SoloninkoOptsPass : public MachineFunctionPass {
+class X86SoloninkoOptsPass : public llvm::MachineFunctionPass {
 public:
   static inline char ID = 0;
 
-  X86SoloninkoOptsPass() : MachineFunctionPass(ID) {}
+  X86SoloninkoOptsPass() : llvm::MachineFunctionPass(ID) {}
 
 private:
-
-  void buildMI(llvm::MachineBasicBlock &MachineBlock, llvm::MachineBasicBlock::iterator &MulInstr, 
-               const llvm::TargetInstrInfo *TII, llvm::MachineBasicBlock::iterator &MBIter) {
-        llvm::MachineInstrBuilder MIB =
-                    BuildMI(MachineBlock, *MulInstr, MulInstr->getDebugLoc(),
-                            TII->get(llvm::X86::VFMADD213PDr));
-                MIB.addReg(MBIter->getOperand(0).getReg(), llvm::RegState::Define);
-                MIB.addReg(MulInstr->getOperand(1).getReg());
-                MIB.addReg(MulInstr->getOperand(2).getReg());
-                MIB.addReg(MBIter->getOperand(2).getReg());
-                MBIter = MachineBlock.erase(MBIter);
-                MulInstr->eraseFromParent();
+  void buildMI(llvm::MachineBasicBlock &MachineBlock,
+               llvm::MachineBasicBlock::iterator &MulInstr,
+               const llvm::TargetInstrInfo *TII,
+               llvm::MachineBasicBlock::iterator &MBIter) {
+    llvm::MachineInstrBuilder MIB =
+        BuildMI(MachineBlock, *MulInstr, MulInstr->getDebugLoc(),
+                TII->get(llvm::X86::VFMADD213PDr));
+    MIB.addReg(MBIter->getOperand(0).getReg(), llvm::RegState::Define);
+    MIB.addReg(MulInstr->getOperand(1).getReg());
+    MIB.addReg(MulInstr->getOperand(2).getReg());
+    MIB.addReg(MBIter->getOperand(2).getReg());
+    MBIter = MachineBlock.erase(MBIter);
+    MulInstr->eraseFromParent();
   }
 
-  bool runOnMachineFunction(MachineFunction &MF) override {
+  bool runOnMachineFunction(llvm::MachineFunction &MF) override {
     for (llvm::MachineBasicBlock &MachineBlock : MF) {
       for (llvm::MachineBasicBlock::iterator MBIter = MachineBlock.begin();
            MBIter != MachineBlock.end(); ++MBIter) {
@@ -40,7 +41,8 @@ private:
             if (MBIter->getOpcode() == llvm::X86::ADDPDrr) {
               if (MulInstr->getOperand(0).getReg() ==
                   MBIter->getOperand(1).getReg()) {
-                const llvm::TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
+                const llvm::TargetInstrInfo *TII =
+                    MF.getSubtarget().getInstrInfo();
                 buildMI(MachineBlock, MulInstr, TII, MBIter);
               }
             } else {
@@ -56,4 +58,4 @@ private:
 };
 
 INITIALIZE_PASS(X86SoloninkoOptsPass, "x86-soloninko-lab3",
-                "X86 Soloninko Pass", false, false)
+                "X86 Soloninko Pass", false, false);
