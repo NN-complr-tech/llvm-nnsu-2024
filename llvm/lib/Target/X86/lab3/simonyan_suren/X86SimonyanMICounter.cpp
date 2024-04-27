7a976331454e9760071702d87df66b4802cbef87
Suren <simonyan-junior@yandex.ru>
2024-04-27 10:55:10 +0300

    Fix

--- llvm/lib/Target/X86/lab3/simonyan_suren/X86SimonyanMICounter.cpp
+++ llvm/lib/Target/X86/lab3/simonyan_suren/X86SimonyanMICounter.cpp
@@ -20,21 +20,6 @@ public:
     const TargetInstrInfo *TII = MF.getSubtarget().getInstrInfo();
     DebugLoc DL3 = MF.front().begin()->getDebugLoc();
 
-    // Создаем новый виртуальный регистр
-    unsigned icReg = MF.getRegInfo().createVirtualRegister(&X86::GR64RegClass);
-
-    // Проверяем, существует ли глобальная переменная 'ic'
-    Module &M = *MF.getFunction().getParent();
-    GlobalVariable *gvar = M.getGlobalVariable("ic");
-
-    // Если 'ic' не существует, создаем ее
-    if (!gvar) {
-      LLVMContext &context = M.getContext();
-      gvar = new GlobalVariable(M, IntegerType::get(context, 64), false,
-                                GlobalValue::ExternalLinkage, nullptr, "ic");
-      gvar->setAlignment(Align(8));
-    }
-
     for (auto &MBB : MF) {
       unsigned count = 0;
       for (auto &MI : MBB) {
@@ -42,25 +27,13 @@ public:
           ++count;
       }
 
-      // Обновляем счетчик
-      BuildMI(MBB, MBB.getFirstTerminator(), DL3, TII->get(X86::ADD64ri32),
-              icReg)
-          .addReg(icReg)
-          .addImm(count);
-    }
-
-    for (auto &MBB : MF) {
-      if (MBB.getFirstTerminator() != MBB.end()) {
-        // Write to global variable ic
-        BuildMI(MBB, MBB.getFirstTerminator(), DL3, TII->get(X86::MOV64mr))
-            .addReg(icReg)
-            .addExternalSymbol("ic");
-      }
+      BuildMI(MBB, MBB.getFirstTerminator(), DL3, TII->get(X86::ADD64ri32))
+          .addImm(count)
+          .addExternalSymbol("ic");
     }
 
     return true;
   }
-
 };
 
 char X86SimonyanMICounterPass::ID = 0;
@@ -68,5 +41,5 @@ char X86SimonyanMICounterPass::ID = 0;
 } // end anonymous namespace
 
 static RegisterPass<X86SimonyanMICounterPass>
-    X("x86-simonyan-mi-counter",
-      "X86 Count of machine instructions pass", false, false);
+    X("x86-simonyan-mi-counter", "X86 Count of machine instructions pass",
+      false, false);
