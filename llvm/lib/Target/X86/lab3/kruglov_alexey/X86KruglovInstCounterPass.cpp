a4b35fdde04c5d32277675b8e8ad3b18774ee5ba
Alex <x.kryack.x@gmail.com>
2024-05-18 20:17:57 +0300

    first ver

---
+++ llvm/lib/Target/X86/lab3/kruglov_alexey/X86KruglovInstCounterPass.cpp
@@ -0,0 +1,39 @@
+#include "X86.h"
+#include "X86InstrInfo.h"
+#include "X86Subtarget.h"
+#include "llvm/CodeGen/MachineFunctionPass.h"
+#include "llvm/CodeGen/MachineInstrBuilder.h"
+
+
+using namespace llvm;
+
+namespace {
+class X86KruglovCntPass : public MachineFunctionPass {
+public:
+  static inline char ID = 0;
+
+  X86KruglovCntPass() : MachineFunctionPass(ID) {}
+
+  bool runOnMachineFunction(MachineFunction &MF) override {
+    DebugLoc DebugLocation = MF.front().begin()->getDebugLoc();
+    const TargetInstrInfo *InstrInfo = MF.getSubtarget().getInstrInfo();
+
+    for (auto &BasicBlock : MF) {
+      size_t Count = 0;
+      for (auto &Instr : BasicBlock) {
+        Count++;
+      }
+        
+      BuildMI(BasicBlock, BasicBlock.getFirstTerminator(), DebugLocation,
+              InstrInfo->get(X86::ADD64ri32))
+          .addImm(Count)
+          .addExternalSymbol("ic");
+    }
+    return true;
+  }
+};
+
+} // end anonymous namespace
+
+static RegisterPass<X86KruglovCntPass>
+    X("x86-kruglov-cnt-pass", "Instruction counter pass", false, false);
