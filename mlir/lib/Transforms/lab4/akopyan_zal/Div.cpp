4442472156793426e3e18c7d07e176b1b5c5a27d
Зал <fartyna-mercedes@mail.ru>
2024-05-18 18:56:05 +0300

    fix clang3

--- mlir/lib/Transforms/lab4/akopyan_zal/Div.cpp
+++ mlir/lib/Transforms/lab4/akopyan_zal/Div.cpp
@@ -1,4 +1,31 @@
-#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
+
+using namespace mlir;
+
+namespace {
+struct DivPass : public PassWrapper<DivPass, OperationPass<ModuleOp>> {
+  void replaceCeilDivUI(arith::CeilDivUIOp op);
+  void replaceCeilDivSI(arith::CeilDivSIOp op);
+  
+  template <typename DivOp>
+  void replaceCeilDiv(Operation *op, StringRef divOpName);
+
+  void runOnOperation() override;
+};
+
+void DivPass::replaceCeilDivUI(arith::CeilDivUIOp op) {
+  replaceCeilDiv<arith::DivUIOp>(op, arith::DivUIOp::getOperationName());
+}
+
+void DivPass::replaceCeilDivSI(arith::CeilDivSIOp op) {
+  replaceCeilDiv<arith::DivSIOp>(op, arith::DivSIOp::getOperationName());
+}
+
+template <typename DivOp>#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
@@ -33,7 +60,7 @@ private:
     replaceCeilDiv(op, arith::DivSIOp::getOperationName());
   }
 
-  template <typename DivOp>
+  template<typename DivOp>
   void replaceCeilDiv(Operation *op, StringRef divOpName) {
     OpBuilder builder(op);
     Location loc = op->getLoc();
@@ -42,7 +69,7 @@ private:
     Type type = a.getType();
 
     Value one = 
-        builder.create<arith::ConstantOp>(loc, builder.getIntegerAttr(type, 1));
+	    builder.create<arith::ConstantOp>(loc, builder.getIntegerAttr(type, 1));
     Value add = builder.create<arith::AddIOp>(loc, a, b);
     Value sub = builder.create<arith::SubIOp>(loc, add, one);
     Value div = builder.create<arith::DivOp>(loc, type, sub, b);
