eee6e636fbd3d5b8813188e8c588dc114f5f15a8
ks2k23 <lbdvks@mail.ru>
2024-05-20 02:47:35 +0300

    lab4_1

---
+++ mlir/lib/Transforms/lab4/lebedeva_ksenia/LebedevaCallFuncCounter.cpp
@@ -0,0 +1,46 @@
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Tools/Plugins/PassPlugin.h"
+
+using namespace mlir;
+
+class LebedevaCallFuncCounter
+    : public PassWrapper<LebedevaCallFuncCounter, OperationPass<ModuleOp>> {
+private:
+    std::map<StringRef, int> сounter;
+public:
+  void runOnOperation() override {
+    getOperation()->walk([&](Operation *op) {
+      if (auto callOp = dyn_cast<LLVM::CallOp>(op)) {
+        StringRef functionName = callOp.getCallee().value();
+        сounter[functionName]++;
+      }
+    });
+    getOperation()->walk([&](Operation *op) {
+      if (auto functionOp = dyn_cast<LLVM::LLVMFuncOp>(op)) {
+        StringRef functionName = functionOp.getName();
+        int nummerCalls = сounter[functionName];
+        auto attrValue = IntegerAttr::get(
+            IntegerType::get(functionOp.getContext(), 32), nummerCalls);
+        functionOp->setAttr("call-count", attrValue);
+      }
+    });
+  }
+  StringRef getArgument() const final { return "lebedeva-call-func-counter"; }
+  StringRef getDescription() const final {
+    return "Simple pass that counts the number of calls to each function";
+  }
+};
+
+MLIR_DECLARE_EXPLICIT_TYPE_ID(LebedevaCallFuncCounter)
+MLIR_DEFINE_EXPLICIT_TYPE_ID(LebedevaCallFuncCounter)
+
+PassPluginLibraryInfo getLebedevaCallFuncCounterPluginInfo() {
+  return {MLIR_PLUGIN_API_VERSION, "lebedeva-call-func-counter",
+          LLVM_VERSION_STRING,
+          []() { PassRegistration<LebedevaCallFuncCounter>(); }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
+  return getLebedevaCallFuncCounterPluginInfo();
+}
