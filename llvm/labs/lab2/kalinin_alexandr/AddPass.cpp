24e269304e23d330d9797662a571ffa6235fb417
Suren <alexkalinin19756@mail.ru>
2024-05-28 10:29:00 +0300

    add cmake

---
+++ llvm/labs/lab2/kalinin_alexandr/AddPass.cpp
@@ -0,0 +1,30 @@
+#include "llvm/Analysis/LoopInfo.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
+
+using namespace llvm;
+
+struct AddPass : public PassInfoMixin<AddPass> {
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+    return PreservedAnalyses::all();
+  }
+};
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+  return {LLVM_PLUGIN_API_VERSION, "AddNewPlugin", LLVM_VERSION_STRING,
+          [](llvm::PassBuilder &PB) {
+            PB.registerPipelineParsingCallback(
+                [](llvm::StringRef Name, llvm::FunctionPassManager &PM,
+                   llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                  if (Name == "loop_func") {
+                    PM.addPass(AddPass());
+                    return true;
+                  }
+                  return false;
+                });
+          }};
+}
