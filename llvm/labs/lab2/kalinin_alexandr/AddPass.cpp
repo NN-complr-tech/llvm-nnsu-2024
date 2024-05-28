2896ffd18de4229f2d444999f49612d9721980ba
Suren <alexkalinin19756@mail.ru>
2024-05-28 10:51:30 +0300

    add loop_start

--- llvm/labs/lab2/kalinin_alexandr/AddPass.cpp
+++ llvm/labs/lab2/kalinin_alexandr/AddPass.cpp
@@ -8,7 +8,35 @@
 using namespace llvm;
 
 struct AddPass : public PassInfoMixin<AddPass> {
-  PreservedAnalyses run(Function &F, FunctionAnalysisManager &FAM) {
+PreservedAnalyses run(Function &Func, FunctionAnalysisManager &FuncAnalysisMgr) {
+
+    LoopInfo &LoopInf = FuncAnalysisMgr.getResult<LoopAnalysis>(Func);
+    auto &Context = Func.getContext();
+    Module *ParentModule = Func.getParent();
+    FunctionType *FuncType = FunctionType::get(Type::getVoidTy(Context), false);
+
+    for (auto &Loop : LoopInf) {
+      BasicBlock *LoopHeader = Loop->getHeader();
+      BasicBlock *PreheaderBlock = Loop->getLoopPreheader();
+      IRBuilder<> IRBuild(LoopHeader->getContext());
+
+      if (PreheaderBlock != nullptr) {
+        bool IsLoopStartFunc = false;
+        for (auto &Inst : *PreheaderBlock) {
+          if (auto *CallInst = dyn_cast<CallInst>(&Inst)) {
+            if (CallInst->getCalledFunction() &&
+                CallInst->getCalledFunction()->getName() == "loop_start") {
+              IsLoopStartFunc = true;
+              break;
+            }
+          }
+        }
+        if (!IsLoopStartFunc) {
+          IRBuild.SetInsertPoint(PreheaderBlock->getTerminator());
+          IRBuild.CreateCall(ParentModule->getOrInsertFunction("loop_start", FuncType));
+        }
+      }
+    }
     return PreservedAnalyses::all();
   }
 };
