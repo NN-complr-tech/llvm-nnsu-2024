1169a791b72e960b5f51bd961af3e26e803ece4c
Suren <alexkalinin19756@mail.ru>
2024-05-28 11:13:47 +0300

    loop_end done

--- llvm/labs/lab2/kalinin_alexandr/AddPass.cpp
+++ llvm/labs/lab2/kalinin_alexandr/AddPass.cpp
@@ -8,19 +8,20 @@
 using namespace llvm;
 
 struct AddPass : public PassInfoMixin<AddPass> {
-PreservedAnalyses run(Function &Func, FunctionAnalysisManager &FuncAnalysisMgr) {
+  PreservedAnalyses run(Function &Func,
+                        FunctionAnalysisManager &FuncAnalysisMgr) {
 
     LoopInfo &LoopInf = FuncAnalysisMgr.getResult<LoopAnalysis>(Func);
     auto &Context = Func.getContext();
     Module *ParentModule = Func.getParent();
     FunctionType *FuncType = FunctionType::get(Type::getVoidTy(Context), false);
 
-    for (auto &Loop : LoopInf) {
+    for (auto &Loop : LoopInf) { // Iterate over all loops in the function
       BasicBlock *LoopHeader = Loop->getHeader();
       BasicBlock *PreheaderBlock = Loop->getLoopPreheader();
       IRBuilder<> IRBuild(LoopHeader->getContext());
 
-      if (PreheaderBlock != nullptr) {
+      if (PreheaderBlock != nullptr) { // Check if the loop has a preheader
         bool IsLoopStartFunc = false;
         for (auto &Inst : *PreheaderBlock) {
           if (auto *CallInst = dyn_cast<CallInst>(&Inst)) {
@@ -31,9 +32,27 @@ PreservedAnalyses run(Function &Func, FunctionAnalysisManager &FuncAnalysisMgr) 
             }
           }
         }
-        if (!IsLoopStartFunc) {
+        if (!IsLoopStartFunc) { // Check if the preheader block has a call to
+                                // loop_start
           IRBuild.SetInsertPoint(PreheaderBlock->getTerminator());
-          IRBuild.CreateCall(ParentModule->getOrInsertFunction("loop_start", FuncType));
+          IRBuild.CreateCall(
+              ParentModule->getOrInsertFunction("loop_start", FuncType));
+        }
+      }
+
+      SmallVector<BasicBlock *, 8> ExitBlocks;
+      Loop->getExitBlocks(ExitBlocks); // Get all exit blocks of the loop
+      bool IsLoopEndFunc = false;
+      for (auto *ExitBlock : ExitBlocks) { // Iterate over all exit blocks
+        IsLoopEndFunc = false;
+        for (auto &Inst : *ExitBlock) {
+          if (auto *CallInst = dyn_cast<CallInst>(&Inst)) {
+            if (CallInst->getCalledFunction() &&
+                CallInst->getCalledFunction()->getName() == "loop_end") {
+              IsLoopEndFunc = true;
+              break;
+            }
+          }
         }
       }
     }
