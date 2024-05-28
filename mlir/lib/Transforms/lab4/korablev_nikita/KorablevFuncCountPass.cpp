11ad2b820b895abc2c437e2b50fb56ca8ec41811
NikitaKorablev <kit06092003@gmail.com>
2024-05-28 23:41:10 +0300

    clang format

--- mlir/lib/Transforms/lab4/korablev_nikita/KorablevFuncCountPass.cpp
+++ mlir/lib/Transforms/lab4/korablev_nikita/KorablevFuncCountPass.cpp
@@ -6,11 +6,13 @@
 using namespace mlir;
 
 namespace {
-class KorablevFuncCountPass: public PassWrapper<KorablevFuncCountPass, OperationPass<ModuleOp>> {
+class KorablevFuncCountPass
+    : public PassWrapper<KorablevFuncCountPass, OperationPass<ModuleOp>> {
 public:
   StringRef getArgument() const final { return "korablev-func-count-pass"; }
   StringRef getDescription() const final {
-    return "A pass that counts the number of times it has been called by other functions in the module";
+    return "A pass that counts the number of times it has been called by other "
+           "functions in the module";
   }
 
   void runOnOperation() override {
@@ -26,8 +28,8 @@ public:
 
     module.walk([&](LLVM::LLVMFuncOp funcOp) {
       StringRef funcName = funcOp.getName();
-      auto attrValue =
-          IntegerAttr::get(IntegerType::get(funcOp.getContext(), 32), callCounter[funcName]);
+      auto attrValue = IntegerAttr::get(
+          IntegerType::get(funcOp.getContext(), 32), callCounter[funcName]);
       funcOp->setAttr("funcCallsCount", attrValue);
     });
   }
@@ -39,7 +41,8 @@ MLIR_DEFINE_EXPLICIT_TYPE_ID(KorablevFuncCountPass)
 
 PassPluginLibraryInfo getKorablevFuncCountPassPluginInfo() {
   return {MLIR_PLUGIN_API_VERSION, "korablev-func-count-pass",
-          LLVM_VERSION_STRING, []() { PassRegistration<KorablevFuncCountPass>(); }};
+          LLVM_VERSION_STRING,
+          []() { PassRegistration<KorablevFuncCountPass>(); }};
 }
 
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
