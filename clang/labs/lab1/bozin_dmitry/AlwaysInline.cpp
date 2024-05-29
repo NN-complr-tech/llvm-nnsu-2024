1544d5be623a5d59bf5fc7456c16bb48381348fb
Nilemos <nilemos@OLEGBASE>
2024-05-29 23:28:17 +0300

    commit1

---
+++ clang/labs/lab1/bozin_dmitry/AlwaysInline.cpp
@@ -0,0 +1,77 @@
+#include "clang/AST/AST.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/Decl.h"
+#include "clang/AST/DeclGroup.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/AST/Stmt.h"
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace {
+    class AlwaysInlineVisitor
+    : public clang::RecursiveASTVisitor<AlwaysInlineVisitor> {
+public:
+  bool VisitStmt(clang::Stmt *Statement) {
+    if (clang::isa<clang::IfStmt>(Statement) || clang::isa<clang::WhileStmt>(Statement) ||
+        clang::isa<clang::ForStmt>(Statement) || clang::isa<clang::DoStmt>(Statement) ||
+        clang::isa<clang::SwitchStmt>(Statement)) {
+      hasCondition = true;
+    }
+    return true;
+  }
+
+  bool hasCondition = false;
+};
+
+
+class AlwaysInlineConsumer : public clang::ASTConsumer {
+public:
+  bool HandleTopLevelDecl(clang::DeclGroupRef DeclGroup) override {
+    for (clang::Decl *Decl : DeclGroup) {
+      if (auto FuncDecl = clang::dyn_cast<clang::FunctionDecl>(Decl)) {
+        if (FuncDecl->getAttr<clang::AlwaysInlineAttr>()) {
+          continue;
+        }
+        clang::Stmt *Body = FuncDecl->getBody();
+        if (Body != nullptr) {
+          AlwaysInlineVisitor Visitor;
+          Visitor.TraverseStmt(Body);
+          if (!Visitor.hasCondition) {
+            clang::SourceLocation Location(
+                FuncDecl->getSourceRange().getBegin());
+            clang::SourceRange Range(Location);
+            FuncDecl->addAttr(clang::AlwaysInlineAttr::Create(
+                FuncDecl->getASTContext(), Range));
+          }
+        }
+      }
+    }
+    return true;
+  }
+};
+
+class BozinAlwaysInlinePlugin : public clang::PluginASTAction {
+protected:
+  std::unique_ptr<clang::ASTConsumer>
+  CreateASTConsumer(clang::CompilerInstance &Compiler,
+                    llvm::StringRef InFile) override {
+    return std::make_unique<AlwaysInlineConsumer>();
+  }
+  bool ParseArgs(const clang::CompilerInstance &Compiler,
+                 const std::vector<std::string> &Args) override {
+    for (const std::string &Arg : Args) {
+      if (Arg == "--help") {
+        llvm::outs() << "adds always_inline if function has no conditions\n";
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+} // namespace
+
+static clang::FrontendPluginRegistry::Add<BozinAlwaysInlinePlugin>
+    X("bozin-always-inline", "adds always_inline if function has no conditions");
---
+++ clang/test/lab1/bozin_dmitry/alwaysinlinetest.cpp
@@ -0,0 +1,103 @@
+// RUN: %clang_cc1 -ast-dump -ast-dump-filter bar -load %llvmshlibdir/BozinAlwaysInlinePlugin%pluginext -add-plugin bozin-always-inline %s | FileCheck %s
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function1 'int \(\)'}}
+int function1() { return 1; }
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function2 'int \(\)'}}
+int function2() {
+  bool flag = false;
+  if (flag) {
+    flag = true;
+  }
+  return 1;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function3 'int \(\)'}}
+int function3() {
+  while (false) {
+  }
+  return 1;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function4 'int \(\)'}}
+int function4() {
+  for (int i = 0; i < 3; i++) {
+  }
+  return 1;
+}
+
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function5 'int \(\)'}}
+int function5() {
+  while (false) {
+    if (true) {
+      int value = 1;
+      if (value) {
+        return value;
+      }
+    }
+  }
+  return 1;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function6 'int \(\)'}}
+int function6() {
+  do {
+
+  } while (false);
+  return 1;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) function7 'int \(\)'}}
+int function7() {
+  int number = 2;
+  switch (number) {
+  case 2:
+    break;
+  default:
+    break;
+  }
+  return number;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo8 'int \(\)'}}
+int function8() {
+  int count;
+  {
+    while (false) {
+      ;
+    }
+  }
+  return 1;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo9 'int \(\)'}}
+int function9() {
+  int count;
+  {
+    {
+      while (false) {
+        ;
+      }
+    }
+  }
+  return 1;
+}
+// CHECK-NOT: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
+
+// CHECK: FunctionDecl {{0[xX][0-9a-fA-F]+ <.+test\.cpp:([0-9]+:[0-9]|[0-9]+), (line|col):([0-9]+:[0-9]|[0-9]+)> (line|col):([0-9]+:[0-9]|[0-9]+) foo10 'int \(int, int\)'}}
+int function10(int number, int value) {
+  int count;
+  count = number - value;
+  return count;
+}
+// CHECK: `-AlwaysInlineAttr {{0[xX][0-9a-fA-F]+ <(line|col):([0-9]+:[0-9]|[0-9]+)> always_inline}}
