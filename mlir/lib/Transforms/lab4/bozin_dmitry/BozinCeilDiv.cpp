4fb673692a3638aee4786a4cc035a5c9030e9923
Nilemos <nilemos@OLEGBASE>
2024-05-30 23:16:09 +0300

    commit2

--- mlir/lib/Transforms/lab4/bozin_dmitry/BozinCeilDiv.cpp
+++ mlir/lib/Transforms/lab4/bozin_dmitry/BozinCeilDiv.cpp
@@ -4,15 +4,17 @@
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
+
 using namespace mlir;
 
 namespace {
-enum class CeilDivisionType { Positive, Negative };
 
-class CeilDivisionPass
-    : public PassWrapper<CeilDivisionPass, OperationPass<LLVM::LLVMFuncOp>> {
+enum class CeilDivType { Signed, Unsigned };
+
+class CeilDivPass
+    : public PassWrapper<CeilDivPass, OperationPass<LLVM::LLVMFuncOp>> {
 public:
-  StringRef getArgument() const final { return "bozin_ceildivisionpass"; }
+  StringRef getArgument() const final { return "bozin_ceildiv"; }
   StringRef getDescription() const final {
     return "breaks arith.ceildivui and arith.ceildivsi operations into arith. "
            "calculations";
@@ -20,17 +22,17 @@ public:
 
   void runOnOperation() override {
     getOperation()->walk([&](Operation *op) {
-      if (auto ceilDivUI = dyn_cast<arith::CeilDivisionUIOp>(op)) {
-        transformCeilDiv(ceilDivUI, CeilDivisionType::Negative);
-      } else if (auto ceilDivSI = dyn_cast<arith::CeilDivisionSIOp>(op)) {
-        transformCeilDiv(ceilDivSI, CeilDivisionType::Positive);
+      if (auto ceilDivUI = dyn_cast<arith::CeilDivUIOp>(op)) {
+        transformCeilDiv(ceilDivUI, CeilDivType::Unsigned);
+      } else if (auto ceilDivSI = dyn_cast<arith::CeilDivSIOp>(op)) {
+        transformCeilDiv(ceilDivSI, CeilDivType::Signed);
       }
     });
   }
 
 private:
-  template <typename CeilDivisionOp>
-  void transformCeilDiv(CeilDivisionOp op, CeilDivisionType divType) {
+  template <typename CeilDivOp>
+  void transformCeilDiv(CeilDivOp op, CeilDivType divType) {
     OpBuilder builder(op);
     Location location = op.getLoc();
     Value numerator = op.getLhs();
@@ -42,7 +44,7 @@ private:
     Value adjustedSum = builder.create<arith::SubIOp>(location, sum, one);
     Value quotient;
 
-    if (divType == CeilDivisionType::Positive) {
+    if (divType == CeilDivType::Signed) {
       quotient =
           builder.create<arith::DivSIOp>(location, adjustedSum, denominator);
     } else {
@@ -61,8 +63,8 @@ MLIR_DECLARE_EXPLICIT_TYPE_ID(CeilDivPass)
 MLIR_DEFINE_EXPLICIT_TYPE_ID(CeilDivPass)
 
 PassPluginLibraryInfo getCeilDivPassPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "bozin_ceildivisionpass", LLVM_VERSION_STRING,
-          []() { PassRegistration<CeilDivisionPass>(); }};
+  return {MLIR_PLUGIN_API_VERSION, "bozin_ceildiv", LLVM_VERSION_STRING,
+          []() { PassRegistration<CeilDivPass>(); }};
 }
 
 extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
--- mlir/test/Transforms/lab4/bozin_dmitry/test.mlir
+++ mlir/test/Transforms/lab4/bozin_dmitry/test.mlir
@@ -1,8 +1,8 @@
-// RUN: mlir-opt -load-pass-plugin=%mlir_lib_dir/BozinCeilDivision%shlibext --pass-pipeline="builtin.module(llvm.func(bozin_ceildivisionpass))" %s | FileCheck %s
+// RUN: mlir-opt -load-pass-plugin=%mlir_lib_dir/BozinCeilDiv%shlibext --pass-pipeline="builtin.module(llvm.func(bozin_ceildiv))" %s | FileCheck %s
 
 module {
-llvm.func @ceildiv1(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
-    // CHECK: llvm.func @ceildiv1(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+llvm.func @foo1(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK: llvm.func @foo1(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
     // CHECK-NEXT: %c1_i32 = arith.constant 1 : i32
     // CHECK-NEXT: %0 = arith.addi %arg0, %arg1 : i32
     // CHECK-NEXT: %1 = arith.subi %0, %c1_i32 : i32
@@ -11,8 +11,8 @@ llvm.func @ceildiv1(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #l
     %0 = arith.ceildivsi %arg0, %arg1 : i32
     llvm.return %0 : i32
 }
-llvm.func @ceildiv2(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
-    // CHECK: llvm.func @ceildiv2(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+llvm.func @foo2(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
+    // CHECK: llvm.func @foo2(%arg0: i32, %arg1: i32) -> i32 attributes {llvm.linkage = #llvm.linkage<external>} {
     // CHECK-NEXT: %c1_i32 = arith.constant 1 : i32
     // CHECK-NEXT: %0 = arith.addi %arg0, %arg1 : i32
     // CHECK-NEXT: %1 = arith.subi %0, %c1_i32 : i32
