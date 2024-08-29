c06d4c8f62020c0c4c5dc906b49407cf9e6f17a2
Alexey131313 <alex6756792@gmail.com>
2024-08-29 18:02:26 +0300

    fix2

--- mlir/lib/Transforms/lab4/pivovarov_alexey/PivovarovMergePass.cpp
+++ mlir/lib/Transforms/lab4/pivovarov_alexey/PivovarovMergePass.cpp
@@ -2,70 +2,59 @@
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
+#include "llvm/Support/Debug.h"
 
 using namespace mlir;
 
 namespace {
-class PivovarovFMAPass
-    : public PassWrapper<PivovarovMergePass,
-                         OperationPass<LLVM::LLVMFuncOp>> {
-public:
-  StringRef getArgument() const final { return "pivovarov_add_merge"; }
-  StringRef getDescription() const final {
-    return "This Pass combines the operations of addition and multiplication "
-           "into one";
-  }
+    class PivovarovMergePass
+        : public PassWrapper<PivovarovMergePass,
+        OperationPass<LLVM::LLVMFuncOp>> {
+    private:
+        void combineMulAdd(LLVM::FAddOp addOp, LLVM::FMulOp mulOp, Value otherOperand) {
+            OpBuilder builder(addOp);
+            Value fmaValue = builder.create<LLVM::FMAOp>(
+                addOp.getLoc(), mulOp.getOperand(0), mulOp.getOperand(1), otherOperand);
+            addOp.replaceAllUsesWith(fmaValue);
+            addOp.erase();
+        }
 
-  void runOnOperation() override {
-    LLVM::LLVMFuncOp func = getOperation();
-    func.walk([&](LLVM::FAddOp AddOperation) {
-      Value AddLeft = AddOperation.getOperand(0);
-      Value AddRight = AddOperation.getOperand(1);
+    public:
+        void runOnOperation() override {
+            LLVM::LLVMFuncOp function = getOperation();
 
-      if (auto MultiplyLeft = AddLeft.getDefiningOp<LLVM::FMulOp>()) {
-        HandleMultiplyOperation(AddOperation, MultiplyLeft, AddRight);
-      } else if (auto MultiplyRight = AddRight.getDefiningOp<LLVM::FMulOp>()) {
-        HandleMultiplyOperation(AddOperation, MultiplyRight, AddLeft);
-      }
-    });
+            function.walk([this](LLVM::FAddOp addOp) {
+                Value lhs = addOp.getOperand(0);
+                Value rhs = addOp.getOperand(1);
 
-    func.walk([](LLVM::FMulOp MultiplyOperation) {
-      if (MultiplyOperation.use_empty()) {
-        MultiplyOperation.erase();
-      }
-    });
-  }
+                if (auto mulOp = lhs.getDefiningOp<LLVM::FMulOp>()) {
+                    combineMulAdd(addOp, mulOp, rhs);
+                }
+                else if (auto mulOp = rhs.getDefiningOp<LLVM::FMulOp>()) {
+                    combineMulAdd(addOp, mulOp, lhs);
+                }
+                });
 
-private:
-  void HandleMultiplyOperation(LLVM::FAddOp &AddOperation,
-                               LLVM::FMulOp &MultiplyOperation,
-                               Value &Operand) {
-    OpBuilder builder(AddOperation);
-    Value FMAOperation = builder.create<LLVM::FMAOp>(
-        AddOperation.getLoc(), MultiplyOperation.getOperand(0),
-        MultiplyOperation.getOperand(1), Operand);
-    AddOperation.replaceAllUsesWith(FMAOperation);
+            function.walk([](LLVM::FMulOp mulOp) {
+                if (mulOp.use_empty()) {
+                    mulOp.erase();
+                }
+                });
+        }
 
-    if (MultiplyOperation.use_empty()) {
-      MultiplyOperation.erase();
-    }
-
-    if (AddOperation.use_empty()) {
-      AddOperation.erase();
-    }
-  }
-};
-} // namespace
+        StringRef getArgument() const final { return "pivovarov-combine-mul-add"; }
+        StringRef getDescription() const final {
+            return "Combines multiplication and addition into a single FMA operation.";
+        }
+    };
 
 MLIR_DECLARE_EXPLICIT_TYPE_ID(PivovarovMergePass)
 MLIR_DEFINE_EXPLICIT_TYPE_ID(PivovarovMergePass)
 
-PassPluginLibraryInfo getFusedMultiplyAddPassPluginInfo() {
-  return {MLIR_PLUGIN_API_VERSION, "pivovarov_add_merge",
-          LLVM_VERSION_STRING,
-          []() { PassRegistration<PivovarovMergePass>(); }};
+extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo
+mlirGetPassPluginInfo() {
+    return { MLIR_PLUGIN_API_VERSION, "pivovarov-combine-mul-add",
+            LLVM_VERSION_STRING,
+            []() { PassRegistration<PivovarovMergePass>(); } };
 }
 
-extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo mlirGetPassPluginInfo() {
-  return getFusedMultiplyAddPassPluginInfo();
-}
