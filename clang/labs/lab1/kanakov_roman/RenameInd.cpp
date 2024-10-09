96d89494015ad116c1d5a412bdc26306c738c925
Ach1L <roman.kanakov.3007@gmail.com>
2024-10-09 14:44:50 +0300

    First lab, clang-format fix 2

--- clang/labs/lab1/kanakov_roman/RenameInd.cpp
+++ clang/labs/lab1/kanakov_roman/RenameInd.cpp
@@ -9,6 +9,7 @@ private:
   clang::Rewriter rewriter_;
   std::string nameOld_;
   std::string nameNew_;
+
 public:
   explicit RenameVisitor(clang::Rewriter rewriter, std::string nameOld,
                          std::string nameNew)
@@ -16,44 +17,44 @@ public:
 
   bool VisitFunctionDecl(clang::FunctionDecl *funcDecl) {
     if (funcDecl->getNameAsString() == nameOld_)
-	  rewriter_.ReplaceText(funcDecl->getNameInfo().getSourceRange(), nameNew_);
-	return true;
+      rewriter_.ReplaceText(funcDecl->getNameInfo().getSourceRange(), nameNew_);
+    return true;
   }
 
   bool VisitVarDecl(clang::VarDecl *varDecl) {
     if (varDecl->getNameAsString() == nameOld_)
       rewriter_.ReplaceText(varDecl->getLocation(),
                             varDecl->getNameAsString().size(), nameNew_);
-	if (varDecl->getType().getAsString() == nameOld_ ||
-	    varDecl->getType().getAsString() == nameOld_ + " *")
+    if (varDecl->getType().getAsString() == nameOld_ ||
+        varDecl->getType().getAsString() == nameOld_ + " *")
       rewriter_.ReplaceText(
           varDecl->getTypeSourceInfo()->getTypeLoc().getBeginLoc(),
           nameOld_.size(), nameNew_);
-	return true;
+    return true;
   }
 
   bool VisitCXXRecordDecl(clang::CXXRecordDecl *recordDecl) {
-	if (nameOld_ == recordDecl->getName()) {
-	  rewriter_.ReplaceText(recordDecl->getLocation(), nameNew_);
-	  const auto destructorDecl = recordDecl->getDestructor();
-	  if (recordDecl->hasUserDeclaredDestructor())
+    if (nameOld_ == recordDecl->getName()) {
+      rewriter_.ReplaceText(recordDecl->getLocation(), nameNew_);
+      const auto destructorDecl = recordDecl->getDestructor();
+      if (recordDecl->hasUserDeclaredDestructor())
         rewriter_.ReplaceText(destructorDecl->getLocation(),
                               nameOld_.size() + 1, "~" + nameNew_);
-	}
-	return true;
+    }
+    return true;
   }
 
   bool VisitDeclRefExpr(clang::DeclRefExpr *refDecl) {
     if (nameOld_ == refDecl->getNameInfo().getAsString())
-	  rewriter_.ReplaceText(refDecl->getNameInfo().getSourceRange(), nameNew_);
-	return true;
+      rewriter_.ReplaceText(refDecl->getNameInfo().getSourceRange(), nameNew_);
+    return true;
   }
 
   bool VisitCXXNewExpr(clang::CXXNewExpr *newExpr) {
-	if (nameOld_ == newExpr->getConstructExpr()->getType().getAsString())
+    if (nameOld_ == newExpr->getConstructExpr()->getType().getAsString())
       rewriter_.ReplaceText(newExpr->getExprLoc(), nameOld_.size() + 4,
                             "new " + nameNew_);
-	return true;
+    return true;
   }
 
   bool save() { return rewriter_.overwriteChangedFiles(); }
@@ -61,6 +62,7 @@ public:
 
 class RenameConsumer : public clang::ASTConsumer {
   RenameVisitor Visitor_;
+
 public:
   explicit RenameConsumer(clang::CompilerInstance &CI, std::string nameOld,
                           std::string nameNew)
@@ -75,35 +77,36 @@ public:
 class RenamePlugin : public clang::PluginASTAction {
   std::string nameOld_;
   std::string nameNew_;
+
 protected:
   bool ParseArgs(const clang::CompilerInstance &CI,
-	const std::vector<std::string> &args) override {
-	if (args.size() < 2) {
-	  llvm::errs() << "Params can't be less then two\n";
-	  return false;
-	}
+                 const std::vector<std::string> &args) override {
+    if (args.size() < 2) {
+      llvm::errs() << "Params can't be less then two\n";
+      return false;
+    }
 
-	for (const auto &arg : args) {
-	  if (arg.find("nameOld_=") == 0)
-		nameOld_ = arg.substr(strlen("nameOld_="));
-	  else if (arg.find("nameNew_=") == 0)
-	    nameNew_ = arg.substr(strlen("nameNew_="));
-	}
+    for (const auto &arg : args) {
+      if (arg.find("nameOld_=") == 0)
+        nameOld_ = arg.substr(strlen("nameOld_="));
+      else if (arg.find("nameNew_=") == 0)
+        nameNew_ = arg.substr(strlen("nameNew_="));
+    }
 
-	if (nameOld_.empty() || nameNew_.empty()) {
-		llvm::errs() << "Params 'nameOld_' and/or 'nameNew_' not found\n";
-		return false;
-	}
-	return true;
-}
+    if (nameOld_.empty() || nameNew_.empty()) {
+      llvm::errs() << "Params 'nameOld_' and/or 'nameNew_' not found\n";
+      return false;
+    }
+    return true;
+  }
 
 public:
   std::unique_ptr<clang::ASTConsumer>
   CreateASTConsumer(clang::CompilerInstance &CI,
                     clang::StringRef InFile) override {
     return std::make_unique<RenameConsumer>(CI, nameOld_, nameNew_);
-}
+  }
 };
 
 static clang::FrontendPluginRegistry::Add<RenamePlugin>
-  X("kanakov-rename-plugin", "Rename variable, function or class");
+    X("kanakov-rename-plugin", "Rename variable, function or class");
