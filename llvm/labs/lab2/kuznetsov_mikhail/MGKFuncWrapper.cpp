c6fd7be8236f53911ffd819c24281454f488103d
kuznetsov-mg <kuznetsov-mg@prima.local>
2024-10-17 15:05:53 +0300

    lab2 extra task done

--- llvm/labs/lab2/kuznetsov_mikhail/MGKFuncWrapper.cpp
+++ llvm/labs/lab2/kuznetsov_mikhail/MGKFuncWrapper.cpp
@@ -21,44 +21,47 @@ struct FunctionInstrumentation : llvm::PassInfoMixin<FunctionInstrumentation> {
     llvm::FunctionCallee endInstr =
         mod->getOrInsertFunction("end_instrument", funcType);
 
-    llvm::Function *startFunc = llvm::dyn_cast<llvm::Function>(
-        startInstr.getCallee()->stripPointerCasts());
-    llvm::Function *endFunc = llvm::dyn_cast<llvm::Function>(
-        endInstr.getCallee()->stripPointerCasts());
+    llvm::Value* val = startInstr.getCallee()->stripPointerCasts();
+    llvm::Value* val1 = endInstr.getCallee()->stripPointerCasts();
+    if (llvm::isa<llvm::Function>(val) && llvm::isa<llvm::Function>(val1)) {
+      llvm::Function *startFunc = llvm::cast<llvm::Function>(val);
+      llvm::Function *endFunc = llvm::cast<llvm::Function>(val1);
 
-    for (auto *user : startFunc->users()) {
-      if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(user)) {
-        if (callInst->getFunction() == &func) {
-          hasStart = true;
-          break;
+      for (auto *user : startFunc->users()) {
+        if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(user)) {
+          if (callInst->getFunction() == &func) {
+            hasStart = true;
+            break;
+          }
         }
       }
-    }
 
-    for (auto *user : endFunc->users()) {
-      if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(user)) {
-        if (callInst->getFunction() == &func) {
-          hasEnd = true;
-          break;
+      for (auto *user : endFunc->users()) {
+        if (auto *callInst = llvm::dyn_cast<llvm::CallInst>(user)) {
+          if (callInst->getFunction() == &func) {
+            hasEnd = true;
+            break;
+          }
         }
       }
-    }
 
-    if (!hasStart) {
-      builder.SetInsertPoint(&func.getEntryBlock().front());
-      builder.CreateCall(startInstr);
-    }
+      if (!hasStart) {
+        builder.SetInsertPoint(&func.getEntryBlock().front());
+        builder.CreateCall(startInstr);
+      }
 
-    if (!hasEnd) {
-      for (llvm::BasicBlock &BB : func) {
-        if (llvm::dyn_cast<llvm::ReturnInst>(BB.getTerminator())) {
-          builder.SetInsertPoint(BB.getTerminator());
-          builder.CreateCall(endInstr);
+      if (!hasEnd) {
+        for (llvm::BasicBlock &BB : func) {
+          if (llvm::dyn_cast<llvm::ReturnInst>(BB.getTerminator())) {
+            builder.SetInsertPoint(BB.getTerminator());
+            builder.CreateCall(endInstr);
+          }
         }
       }
+      return llvm::PreservedAnalyses::all();
     }
 
-    return llvm::PreservedAnalyses::all();
+    return llvm::PreservedAnalyses::none();
   }
 
   static bool isRequired() { return true; }
